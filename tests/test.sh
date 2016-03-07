8595a46cd22d81ffbaa223fbaabcddf626c8927b
Stefan Micheelsen <stufkan@gmail.com>
2016-03-07 20:05:54 +0100

    labels for each test

--- tests/test.sh
+++ tests/test.sh
@@ -1,3 +1,10 @@
+echo "LabelVisitor:"
 python -m unittest label_visitor_test.py
+
+echo ""
+echo "VarsVisitor:"
 python -m unittest vars_visitor_test.py
+
+echo ""
+echo "CFG:"
 python -m unittest cfg_creator_test.py
