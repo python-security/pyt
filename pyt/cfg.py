029508ff90603240ea8bf7d91c20f64f42bc2047
Stefan Micheelsen <stufkan@gmail.com>
2016-03-08 16:14:17 +0100

    extract while and for code into a skeleton used by both

--- pyt/cfg.py
+++ pyt/cfg.py
@@ -185,8 +185,7 @@ class CFG(ast.NodeVisitor):
 
         return n
 
-    def visit_While(self, node): 
-        test = self.visit(node.test)
+    def loop_node_skeleton(self, test, node):
         body_stmts = self.stmt_star_handler(node.body)
 
         body_first = body_stmts[0]
@@ -209,41 +208,22 @@ class CFG(ast.NodeVisitor):
             body_last.outgoing.append(orelse_first)
             last_nodes.append(orelse_last)
 
-
         return (test,last_nodes)
+    
+    def visit_While(self, node):
+        test = self.visit(node.test)
+        return self.loop_node_skeleton(test, node)
+
+
+
 
     def visit_For(self, node):
         target = self.visit(node.target)
-        iter = self.visit(node.iter)
-
-        for_node = Node("for " + target.label + " in " + iter.label, node.__class__.__name__)
+        iterator = self.visit(node.iter)
+        for_node = Node("for " + target.label + " in " + iterator.label, node.__class__.__name__)
         self.nodes.append(for_node)
         
-        body_stmts = self.stmt_star_handler(node.body)
-
-        body_first = body_stmts[0]
-        for_node.outgoing.append(body_first)
-        
-        body_last = body_stmts[-1]
-        body_last.outgoing.append(for_node)
-
-        # last_nodes is used for making connections to the next node in the parent node
-        # this is handled in stmt_star_handler
-        last_nodes = list() 
-        last_nodes.append(body_last)
-        
-        if node.orelse:
-            orelse_stmts = self.stmt_star_handler(node.orelse)
-            orelse_last = orelse_stmts[-1]
-            orelse_first = orelse_stmts[0]
-
-            for_node.outgoing.append(orelse_first)
-            body_last.outgoing.append(orelse_first)
-            last_nodes.append(orelse_last)
-
-
-        return (for_node,last_nodes)
-
+        return self.loop_node_skeleton(for_node, node)
 
     def visit_Compare(self, node):
         
