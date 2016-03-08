89fa30c4c7c9d79ebb3ff5248033d3b0100d5baa
Thalmann <mr.thalmann@gmail.com>
2016-03-08 11:08:48 +0100

    Easy to add tests to the test suite now.

--- tests/test.py
+++ tests/test.py
@@ -1,17 +1,24 @@
 from unittest import TextTestRunner
 from unittest import TestSuite
 from unittest import makeSuite
-from unittest import defaultTestLoader as loader
 
 from label_visitor_test import LabelVisitorTest
 from vars_visitor_test import VarsVisitorTest
+from cfg_creator_test import CFGIfTest, CFGWhileTest
+
 
 test_suite = TestSuite()
 
 # Add TestCase's here
-test_suite.addTest(makeSuite(LabelVisitorTest))
-test_suite.addTest(makeSuite(VarsVisitorTest))
+tests = [
+    LabelVisitorTest,
+    VarsVisitorTest,
+    CFGIfTest,
+    CFGWhileTest
+]
 
+for test in tests:
+    test_suite.addTest(makeSuite(test))
 
 runner = TextTestRunner(verbosity=2)
 runner.run(test_suite)
