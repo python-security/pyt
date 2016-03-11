a94e808d8ca45cf4696a1084f5dc552ed77dd384
Thalmann <mr.thalmann@gmail.com>
2016-03-11 12:26:25 +0100

    initialize cfg in setup method

--- tests/fixed_point_test.py
+++ tests/fixed_point_test.py
@@ -8,20 +8,26 @@ from cfg import CFG, generate_ast, Node
 from fixed_point import fixpoint_iteration
 
 class FixedPointTestCase(unittest.TestCase):
-    def test_fixpoint_algorithm(self):
+    def setUp(self):
         tree = generate_ast('../example/example_inputs/example.py')
-        cfg = CFG()
-        cfg.create(tree)
-        fixpoint_iteration(cfg)
+        self.cfg = CFG()
+        self.cfg.create(tree)
+        
+    def test_fixpoint_algorithm_first_iteration(self):
+
+        fixpoint_iteration(self.cfg)
 
 
-        self.assertIn(cfg.nodes[1], cfg.nodes[1].new_constraint)
-        self.assertIn(cfg.nodes[2], cfg.nodes[2].new_constraint)
-        self.assertIn(cfg.nodes[4], cfg.nodes[4].new_constraint)
-        self.assertIn(cfg.nodes[6], cfg.nodes[6].new_constraint)
-        self.assertIn(cfg.nodes[7], cfg.nodes[7].new_constraint)
-        self.assertIn(cfg.nodes[9], cfg.nodes[9].new_constraint)
-        self.assertIn(cfg.nodes[10], cfg.nodes[10].new_constraint)
+        self.assertIn(self.cfg.nodes[1], self.cfg.nodes[1].new_constraint)
+        self.assertIn(self.cfg.nodes[2], self.cfg.nodes[2].new_constraint)
+        self.assertIn(self.cfg.nodes[4], self.cfg.nodes[4].new_constraint)
+        self.assertIn(self.cfg.nodes[6], self.cfg.nodes[6].new_constraint)
+        self.assertIn(self.cfg.nodes[7], self.cfg.nodes[7].new_constraint)
+        self.assertIn(self.cfg.nodes[9], self.cfg.nodes[9].new_constraint)
+        self.assertIn(self.cfg.nodes[10], self.cfg.nodes[10].new_constraint)
+
+        for empty in [3,5,8,11,12]:
+            self.assertEqual(self.cfg.nodes[empty].new_constraint, set())
 
         for empty in [3,5,8,11,12]:
             self.assertEqual(cfg.nodes[empty].new_constraint, set())
