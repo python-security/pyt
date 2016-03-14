3903c4761b0e49a4150998cde88d7910d228fffc
Stefan Micheelsen <stufkan@gmail.com>
2016-03-14 17:29:36 +0100

    create method for testing a list of tuples, as well as all unlisted tuples for easier test setup

--- tests/fixed_point_test.py
+++ tests/fixed_point_test.py
@@ -2,55 +2,54 @@ import os
 import sys
 import unittest
 from ast import parse
+from collections import namedtuple
 
 sys.path.insert(0, os.path.abspath('../pyt'))
 from cfg import CFG, generate_ast, Node
 from fixed_point import fixpoint_iteration, swap_constraints
 
 class FixedPointTestCase(unittest.TestCase):
+    connection = namedtuple('connection', 'constraintset element')
     def setUp(self):
         tree = generate_ast('../example/example_inputs/example.py')
         self.cfg = CFG()
         self.cfg.create(tree)
+
+    def assertInCfg(self, connections):
+        ''' Assert that all connections in the connections list exists in the cfg,
+        as well as all connections not in the list do not exist
+
+        connections is a list of tuples where the node at index 0 of the tuple has to be in the new_constraintset of the node a index 1 of the tuple'''
+        for connection in connections:
+            self.assertIn(self.cfg.nodes[connection[0]], self.cfg.nodes[connection[1]].new_constraint)
+
+        nodes = len(self.cfg.nodes)
+        
+        for element in range(nodes):
+            for sets in range(nodes):
+                if (element, sets) not in connections:
+                    self.assertNotIn(self.cfg.nodes[element], self.cfg.nodes[sets].new_constraint)
         
     def test_fixpoint_algorithm_first_iteration(self):
-
         fixpoint_iteration(self.cfg)
 
 
-        self.assertIn(self.cfg.nodes[1], self.cfg.nodes[1].new_constraint)
-        self.assertIn(self.cfg.nodes[2], self.cfg.nodes[2].new_constraint)
-        self.assertIn(self.cfg.nodes[4], self.cfg.nodes[4].new_constraint)
-        self.assertIn(self.cfg.nodes[6], self.cfg.nodes[6].new_constraint)
-        self.assertIn(self.cfg.nodes[7], self.cfg.nodes[7].new_constraint)
-        self.assertIn(self.cfg.nodes[9], self.cfg.nodes[9].new_constraint)
-        self.assertIn(self.cfg.nodes[10], self.cfg.nodes[10].new_constraint)
-
-        for empty in [0,3,5,8,11,12]:
-            self.assertEqual(self.cfg.nodes[empty].new_constraint, set())
-
+        self.assertInCfg([(1,1),(2,2),(4,4),(6,6),(7,7),(9,9),(10,10)])
+        
     def test_fixpoint_algorithm_second_iteration(self):
         fixpoint_iteration(self.cfg)
         swap_constraints(self.cfg)
         fixpoint_iteration(self.cfg)
 
-
-        self.assertIn(self.cfg.nodes[1], self.cfg.nodes[1].new_constraint)
-        self.assertIn(self.cfg.nodes[2], self.cfg.nodes[2].new_constraint)
-        self.assertIn(self.cfg.nodes[2], self.cfg.nodes[3].new_constraint)
-        self.assertIn(self.cfg.nodes[10], self.cfg.nodes[3].new_constraint)
-        self.assertIn(self.cfg.nodes[4], self.cfg.nodes[4].new_constraint)
-        self.assertIn(self.cfg.nodes[4], self.cfg.nodes[5].new_constraint)
-        self.assertIn(self.cfg.nodes[6], self.cfg.nodes[6].new_constraint)
-        self.assertIn(self.cfg.nodes[6], self.cfg.nodes[7].new_constraint)
-        self.assertIn(self.cfg.nodes[7], self.cfg.nodes[7].new_constraint)
-        self.assertIn(self.cfg.nodes[7], self.cfg.nodes[8].new_constraint)
-        self.assertIn(self.cfg.nodes[9], self.cfg.nodes[9].new_constraint)
-        self.assertIn(self.cfg.nodes[9], self.cfg.nodes[10].new_constraint)
-        self.assertIn(self.cfg.nodes[10], self.cfg.nodes[10].new_constraint)
-        self.assertIn(self.cfg.nodes[10], self.cfg.nodes[11].new_constraint)
-
-        for empty in [0, 12]:
-            for x in self.cfg.nodes[empty].new_constraint:
-                print(x)
-            self.assertEqual(self.cfg.nodes[empty].new_constraint, set())
+        self.assertInCfg([(1,1),
+                          (2,2),
+                          (2,3),(10,3),
+                          (4,4),
+                          (4,5),
+                          (6,6),
+                          (6,7),(7,7),
+                          (7,8),
+                          (9,9),
+                          (9,10),(10,10),
+                          (10,11)])
+        
