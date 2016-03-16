bdc15b891cda356e2c3d54f263388181d974269c
Stefan Micheelsen <stufkan@gmail.com>
2016-03-16 20:26:27 +0100

    example file for testing scope if becomes necessary in the future

---
+++ example/example_inputs/multiscope.py
@@ -0,0 +1,14 @@
+y = 1
+for x in range(5):
+    g = 10
+    if x > y:
+        while y < 5:
+            print( x)
+            x += 1
+        else:
+            print(y + x)
+    else:
+        print(x*2)
+        h = g * 2
+
+print(x)
