054cdd1a3f9219ad9fd53aab2c0580b7c95b8a85
Stefan Micheelsen <stufkan@gmail.com>
2016-03-21 20:41:42 +0100

    add tests to test script

--- tests/test.py
+++ tests/test.py
@@ -4,7 +4,7 @@ from unittest import makeSuite
 
 from label_visitor_test import LabelVisitorTest
 from vars_visitor_test import VarsVisitorTest
-from cfg_test import CFGIfTest, CFGWhileTest, CFGStartExitNodeTest, CFGForTest, CFGGeneralTest, CFGFunctionNodeTest, CFGFunctionParameterNodeTest, CFGAssignemntAndBuiltinTest
+from cfg_test import CFGIfTest, CFGWhileTest, CFGStartExitNodeTest, CFGForTest, CFGGeneralTest, CFGFunctionNodeTest, CFGFunctionParameterNodeTest, CFGAssignemntAndBuiltinTest, CFGFunctionNodeWithReturnTest
 from reaching_definitions_test import FixedPointTest
 
 test_suite = TestSuite()
@@ -19,7 +19,8 @@ tests = [
     CFGForTest,
     CFGGeneralTest,
     FixedPointTest,
-    CFGAssignemntAndBuiltinTest
+    CFGAssignemntAndBuiltinTest,
+    CFGFunctionNodeWithReturnTest
 ]
 
 for test in tests:
