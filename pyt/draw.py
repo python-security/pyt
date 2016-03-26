db8da76170eb0b992fb4fd053ef26f9f1d1fd80f
Thalmann <mr.thalmann@gmail.com>
2016-03-26 22:19:18 +0100

    Added style and helper function from doc
    We can use this for making our own style and shit

--- pyt/draw.py
+++ pyt/draw.py
@@ -1,5 +1,44 @@
 from graphviz import Digraph
 
+styles = {
+    'graph': {
+        'label': 'A Fancy Graph',
+        'fontsize': '16',
+        'fontcolor': 'white',
+        'bgcolor': '#333333',
+        'rankdir': 'BT',
+    },
+    'nodes': {
+        'fontname': 'Helvetica',
+        'shape': 'hexagon',
+        'fontcolor': 'white',
+        'color': 'white',
+        'style': 'filled',
+        'fillcolor': '#006699',
+    },
+    'edges': {
+        'style': 'dashed',
+        'color': 'white',
+        'arrowhead': 'open',
+        'fontname': 'Courier',
+        'fontsize': '12',
+        'fontcolor': 'white',
+    }
+}
+
+def apply_styles(graph, styles):
+    graph.graph_attr.update(
+        ('graph' in styles and styles['graph']) or {}
+    )
+    graph.node_attr.update(
+        ('nodes' in styles and styles['nodes']) or {}
+    )
+    graph.edge_attr.update(
+        ('edges' in styles and styles['edges']) or {}
+    )
+    return graph
+
+
 def draw_cfg(cfg, output_filename = 'output'):
     graph = Digraph(format='svg')
     
@@ -8,4 +47,5 @@ def draw_cfg(cfg, output_filename = 'output'):
         for ingoing_node in node.ingoing:
             graph.edge(ingoing_node.label, node.label)
 
+    graph = apply_styles(graph, styles)
     graph.render(filename = output_filename)
