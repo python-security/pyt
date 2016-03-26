25916a9da8c3bf755a61d796c01b4494dddd9f99
Thalmann <mr.thalmann@gmail.com>
2016-03-26 20:47:33 +0100

    Added simple arguments for printing cfg and inputting filename

--- pyt/pyt.py
+++ pyt/pyt.py
@@ -1,4 +1,24 @@
 import argparse
+from cfg import generate_ast, CFG
 
 parser = argparse.ArgumentParser()
-parser.parse_args()
+
+parser.add_argument('filename', help = 'Filename of the file that should be analysed.', type = str)
+parser.add_argument('-d', '--draw-cfg', help = 'Draw CFG and output as .svg file.', action='store_true')
+parser.add_argument('-p', '--print', help = 'Prints the nodes of the CFG.', action='store_true')
+parser.add_argument('-vp', '--verbose-print', help = 'Prints the nodes of the CFG verbosely.', action='store_true')
+
+args = parser.parse_args()
+
+if __name__ == '__main__':
+    tree = generate_ast(args.filename)
+    cfg = CFG()
+    cfg.create(tree)
+
+    if args.draw_cfg:
+        pass
+    if args.print:
+        print(cfg)
+    if args.verbose_print:
+        print(repr(cfg))
+    
