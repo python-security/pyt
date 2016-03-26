0bf3f8fe6df5858a57ad92c1e3687ba1030d81d3
Thalmann <mr.thalmann@gmail.com>
2016-03-26 21:51:48 +0100

    Draw function for cfg - NOT TESTED

--- pyt/draw.py
+++ pyt/draw.py
@@ -1,1 +1,11 @@
 from graphviz import Digraph
+
+def draw_cfg(cfg, output_filename = 'output'):
+    graph = Digraph(format='svg')
+    
+    for x, node in enumerate(cfg.nodes):
+        graph.node(node.label)
+        for ingoing_node in node.ingoing:
+            graph.edge(ingoing_node.label, node.label)
+
+    graph.render(filename = output_filename)
