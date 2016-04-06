2d5bb9af7bea9138ee00327af487e2ea2fd66881
Stefan Micheelsen <stufkan@gmail.com>
2016-04-06 12:24:08 +0200

    use TestLoad for test discovery
    So we don't forget more tests!

--- tests/test.py
+++ tests/test.py
@@ -1,40 +1,16 @@
 from unittest import TextTestRunner
 from unittest import TestSuite
 from unittest import makeSuite
+from unittest import TestLoader
 
-from label_visitor_test import LabelVisitorTest
-from vars_visitor_test import VarsVisitorTest
-from cfg_test import CFGIfTest, CFGWhileTest, CFGForTest, CFGGeneralTest, CFGFunctionNodeTest, CFGFunctionParameterNodeTest, CFGAssignmentAndBuiltinTest, CFGFunctionNodeWithReturnTest, CFGMultipleParametersTest, CFGStr, CFGNameConstant, CFGName, CFGAssignmentMultiTargetTest, CFGCallWithAttributeTest, CFGAssignListComprehension
-from reaching_definitions_test import FixedPointTest
-from flask_engine_test import FlaskEngineTest
 test_suite = TestSuite()
 
-# Add TestCase's here
-tests = [
-    LabelVisitorTest,
-    VarsVisitorTest,
-    CFGIfTest,
-    CFGWhileTest,
-    CFGForTest,
-    CFGGeneralTest,
-    FixedPointTest,
-    CFGAssignmentAndBuiltinTest,
-    CFGFunctionNodeWithReturnTest,
-    CFGMultipleParametersTest,
-    CFGStr,
-    CFGNameConstant,
-    CFGName,
-    CFGAssignmentMultiTargetTest,
-    CFGCallWithAttributeTest,
-    FlaskEngineTest,
-    CFGAssignListComprehension
-]
 
-for test in tests:
-    test_suite.addTest(makeSuite(test))
+loader = TestLoader()
+suite = loader.discover('.',pattern='*_test.py')
 
 runner = TextTestRunner(verbosity=2)
-result = runner.run(test_suite)
+result = runner.run(suite)
 if result.wasSuccessful():
     print('Success')
     exit(0)
