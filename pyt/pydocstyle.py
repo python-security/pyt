f5557c422cade1d29cf92362b6ebb28e97e94216
Stefan Micheelsen <stufkan@gmail.com>
2016-04-07 10:21:30 +0200

    script for sorting out visitmethods in pydocstring output

---
+++ pyt/pydocstyle.py
@@ -0,0 +1,18 @@
+import subprocess
+import re
+from pprint import pprint
+
+docstyle = subprocess.run(["pydocstyle", "--ignore=D105"], stderr=subprocess.PIPE, universal_newlines=True)
+
+lines = re.split('\n', docstyle.stderr)
+
+errors = zip(lines[0::2], lines[1::2])
+
+errors = [x + "\n\t" + y for x,y in errors]
+
+errors = [error for error in errors if 'visit_' not in error]
+
+for error in errors:
+    print(error + '\n')
+
+print("Total errors: {}".format(len(errors)))
