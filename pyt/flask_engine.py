0ed1477976a8ef504071b163b4a26995276ad5c9
Thalmann <mr.thalmann@gmail.com>
2016-04-12 15:56:10 +0200

    Path fixed again :O

--- pyt/flask_engine.py
+++ pyt/flask_engine.py
@@ -17,7 +17,7 @@ SinkOrSourceNode = namedtuple('SinkOrSourceNode', 'trigger_word cfg_node')
 
 def parse_sources_and_sinks(trigger_word_file):
     file_data = ''
-    path = os.path.join(s, trigger_word_file)
+    path = os.path.join(os.getcwd(), trigger_word_file)
     with open(path, 'r') as fd:
         is_source = None
         for line in fd:
