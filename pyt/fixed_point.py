08690ab2147c43965eb6e61df49da16603ed34c2
Stefan Micheelsen <stufkan@gmail.com>
2016-04-14 13:48:59 +0200

    make the analysis module changeable in fixed_point

--- pyt/fixed_point.py
+++ pyt/fixed_point.py
@@ -1,43 +1,49 @@
 import ast
 from cfg import Node, AssignmentNode, CFG, generate_ast
 from reaching_definitions import reaching_definitions_analysis
+from liveness import liveness_analysis
 
 class fixed_point_analysis(object):
 
-    def __init__(self, analysis):
+    def __init__(self, cfg, analysis):
         '''Fixed point analysis
 
         analysis must be a dataflow analysis containing a 'fixpointmethod' method that analyzes one CFG node'''
-        self.analysis = analysis()
+        self.analysis = analysis(cfg)
+        self.cfg = cfg
     
-    def constraints_changed(self, cfg):
-        return any(node.old_constraint != node.new_constraint for node in cfg.nodes)
+    def constraints_changed(self):
+        return any(node.old_constraint != node.new_constraint for node in self.cfg.nodes)
         
-    def swap_constraints(self, cfg):
-        for node in cfg.nodes:
+    def swap_constraints(self):
+        for node in self.cfg.nodes:
             node.old_constraint = node.new_constraint
             node.new_constraint = None
             
-    def fixpoint_runner(self, cfg):
-        self.fixpoint_iteration(cfg)
-        while self.constraints_changed(cfg):
-            self.swap_constraints(cfg)
-            self.fixpoint_iteration(cfg)
+    def fixpoint_runner(self):
+        self.fixpoint_iteration()
+        while self.constraints_changed():
+            self.swap_constraints()
+            self.fixpoint_iteration()
         
-    def fixpoint_iteration(self, cfg):
-        for node in cfg.nodes:
+    def fixpoint_iteration(self):
+        for node in self.cfg.nodes:
             self.analysis.fixpointmethod(node)
             
 
 def analyse(cfg_list, *, analysis_type):
-    analysis = fixed_point_analysis(reaching_definitions_analysis)
     for cfg in cfg_list:
-        analysis.fixpoint_runner(cfg)
+        analysis = fixed_point_analysis(cfg, analysis_type)
+        analysis.fixpoint_runner()
 
 if __name__ == '__main__':
-    analysis = fixed_point_analysis(reaching_definitions_analysis)
     tree = generate_ast('../example/example_inputs/example.py')
     cfg = CFG()
     cfg.create(tree)
-    
-    analysis.fixpoint_runner(cfg)
+
+    analysis = fixed_point_analysis(cfg, liveness_analysis)
+    analysis.fixpoint_runner()
+
+    for cfg_node in cfg.nodes:
+        print(cfg_node)
+        print(cfg_node.new_constraint)
