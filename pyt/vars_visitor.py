02a5a1eedcd6cfb0a1f8ca0b619958b2743dfeb3
Stefan Micheelsen <stufkan@gmail.com>
2016-04-14 13:48:14 +0200

    correct the implementation of vars visitor

--- pyt/vars_visitor.py
+++ pyt/vars_visitor.py
@@ -4,15 +4,24 @@ class VarsVisitor(NodeVisitor):
     def __init__(self):
         self.result = list()
 
-    def visit_Name(self,node):
+    def visit_Name(self, node):
         self.result.append(node.id)
 
-    def visit_Call(self,node):
+    #  Condition and call rule
+    def visit_Call(self, node):
         for arg in node.args:
             self.visit(arg)
         for keyword in node.keywords:
             self.visit(keyword)
-
+            
     def visit_keyword(self, node):
         self.visit(node.value)
 
+    def visit_Compare(self, node):
+        self.generic_visit(node)
+
+    #  Assignment rule                
+    def visit_Assign(self, node): 
+        self.visit(node.value)
+
+        
