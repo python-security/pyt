d3275172d2010495772cf0536b48988d029f10ba
Thalmann <mr.thalmann@gmail.com>
2016-04-19 15:45:02 +0200

    AnalysisBase documented

--- pyt/analysis_base.py
+++ pyt/analysis_base.py
@@ -1,14 +1,18 @@
+"""Thos module contains a base class for the analysis component used in PyT."""
+
 class AnalysisBase(object):
     """Base class for fixed point analyses."""
 
     annotated_cfg_nodes = dict()
     
     def __init__(self, cfg, visitor):
+        """Annotate visitor if not None and save visitor."""
         if visitor:
             self.annotate_cfg(cfg, visitor)
         self.visitor = visitor
 
     def annotate_cfg(self, cfg, visitor):
+        """Add the visitor to the cfg nodes."""
         for node in cfg.nodes:
             if node.ast_node:
                 _visitor = visitor()
