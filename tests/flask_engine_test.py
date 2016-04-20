180421f64df9ecfb3391b1f1aa1abc0e8ded106e
Stefan Micheelsen <stufkan@gmail.com>
2016-04-20 10:43:29 +0200

    fix imports and inheritance

--- tests/flask_engine_test.py
+++ tests/flask_engine_test.py
@@ -1,13 +1,12 @@
 import os
 import sys
-import unittest
-from ast import parse
 
 sys.path.insert(0, os.path.abspath('../pyt'))
 from cfg import CFG, generate_ast, Node
 from flask_engine import FlaskEngine
+from base_test_case import BaseTestCase
 
-class FlaskEngineTest(unittest.TestCase):
+class FlaskEngineTest(BaseTestCase):
     def test_find_flask_functions(self):
         self.cfg = CFG()
         tree = generate_ast('../example/example_inputs/flask_function_and_normal_function.py')
@@ -19,7 +18,3 @@ class FlaskEngineTest(unittest.TestCase):
         #self.assertEqual(len(flask_functions), 1)
 
         #self.assertEqual(flask_functions[0], 'flask_function')
-                
-
-
-        
