aa0362a9796433205f50bb28fc8a2edae47d03a1
Thalmann <mr.thalmann@gmail.com>
2016-04-26 11:24:50 +0200

    Sanitised version of XSS

---
+++ example/vulnerable_code/XSS_sanitised.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, make_response, Markup
+import random
+import string
+app = Flask(__name__)
+
+@app.route('/XSS_param', methods =['GET'])
+def XSS1():
+    param = request.args.get('param', 'not set')
+
+    param = Markup.escape(param)
+
+    html = open('templates/XSS_param.html').read()
+    resp = make_response(html.replace('{{ param }}', param))
+    return resp
+
+if __name__ == '__main__':
+    app.run(debug= True)
