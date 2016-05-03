aab08b078b8282c9134a25f68dad18399c8c0a2b
Stefan Micheelsen <stufkan@gmail.com>
2016-05-03 13:00:10 +0200

    document vulnerabilities.py

--- pyt/vulnerabilities.py
+++ pyt/vulnerabilities.py
@@ -1,3 +1,5 @@
+"""Module for finding vulnerabilities based on a definitions file."""
+
 import os
 from collections import namedtuple
 
@@ -18,6 +20,14 @@ SOURCES_KEYWORD = 'sources:'
 SINKS_KEYWORD = 'sinks:'
 
 def parse_section(iterator):
+    """Parse a section of a file. Stops at empty line.
+
+    Args:
+        iterator(File): file descriptor pointing at a definition file.
+
+    Returns:
+         Iterator of all definitions in the section.
+    """
     try:
         line = next(iterator).rstrip()
         while line:
@@ -34,6 +44,11 @@ def parse_section(iterator):
         return
         
 def parse(trigger_word_file=default_trigger_word_file):
+    """Parse the file for source and sink definitions.
+
+    Returns:
+       A definitions tuple with sources and sinks.
+    """
     sources = list()
     sinks = list()
     with open(trigger_word_file, 'r') as fd:
@@ -46,6 +61,16 @@ def parse(trigger_word_file=default_trigger_word_file):
     return Definitions(sources, sinks)
         
 def identify_triggers(cfg, sources, sinks):
+    """Identify sources, sinks and sanitisers in a CFG.
+
+    Args:
+        cfg(CFG): CFG to find sources, sinks and sanitisers in.
+        sources(tuple): list of sources, a source is a (source, sanitiser) tuple.
+        sinks(tuple): list of sources, a sink is a (sink, sanitiser) tuple.
+
+    Returns:
+        Triggers tuple with sink and source nodes and a sanitiser node dict.
+    """
     sources_in_file = find_triggers(cfg, sources)
     sinks_in_file = find_triggers(cfg, sinks)
     
@@ -54,12 +79,30 @@ def identify_triggers(cfg, sources, sinks):
     return Triggers(sources_in_file, sinks_in_file, sanitiser_node_dict)
 
 def find_triggers(cfg, trigger_word_list):
+    """Find triggers from the trigger_word_list in the cfg.
+
+    Args:
+        cfg(CFG): the CFG to find triggers in.
+        trigger_word_list(list[string]): list of trigger words to look for.
+
+    Returns:
+        List of found TriggerNodes
+    """
     l = list()
     for node in cfg.nodes:
         l.extend(iter(label_contains(node, trigger_word_list)))
     return l
       
 def label_contains(node, trigger_words):
+    """Determine if node contains any of the trigger_words provided.
+
+    Args:
+        node(Node): CFG node to check.
+        trigger_words(list[string]): list of trigger words to look for.
+
+    Returns:
+        Iterable of TriggerNodes found. Can be multiple because multiple trigger_words can be in one node.
+    """
     for trigger_word_tuple in trigger_words:
         if trigger_word_tuple[0] in node.label:
             trigger_word = trigger_word_tuple[0]
@@ -67,6 +110,15 @@ def label_contains(node, trigger_words):
             yield TriggerNode(TriggerWordTuple(trigger_word, sanitisers), node)
 
 def build_sanitiser_node_dict(cfg, sinks_in_file):
+    """Build a dict of string -> TriggerNode pairs, where the string is the sanitiser and the TriggerNode is a TriggerNode of the sanitiser.
+
+    Args:
+        cfg(CFG): cfg to traverse.
+        sinks_in_file(list[TriggerNode]): list of TriggerNodes contains the sinks in the file
+
+    Returns:
+        A string -> TriggerNode dict.
+    """
     sanitisers = list()
     for sink in sinks_in_file:
         sanitisers.extend(sink.trigger_word_tuple.sanitisers)
@@ -83,11 +135,29 @@ def build_sanitiser_node_dict(cfg, sinks_in_file):
     return sanitiser_node_dict
 
 def find_sanitiser_nodes(sanitiser, sanitisers_in_file):
+    """Find nodes containing a particular sanitiser.
+
+    Args:
+        sanitiser(string): sanitiser to look for.
+        sanitisers_in_file(list[Node]): list of CFG nodes with the sanitiser.
+
+    Returns:
+        Iterable of sanitiser nodes.
+    """
     for sanitiser_tuple  in sanitisers_in_file:
         if sanitiser == sanitiser_tuple.trigger_word:
             yield sanitiser_tuple.cfg_node
 
 def is_sanitized(sink, sanitiser_dict):
+    """Check if sink is sanitised by any santiser in the sanitiser_dict.
+
+    Args:
+        sink(TriggerNode): TriggerNode of the sink.
+        sanitiser_dict(dict): dictionary of sink sanitiser pairs.
+
+    Returns: 
+        True of False
+    """
     for sanitiser in sink.trigger_word_tuple.sanitisers:
         for cfg_node in sanitiser_dict[sanitiser]:
             if cfg_node in sink.cfg_node.new_constraint:
@@ -95,6 +165,18 @@ def is_sanitized(sink, sanitiser_dict):
     return False
 
 def get_vulnerability(source, sink, triggers):
+    """Get vulnerability between source and sink if it exists.
+
+    Uses triggers to find sanitisers
+
+    Args:
+        source(TriggerNode): TriggerNode of the source.
+        sink(TriggerNode): TriggerNode of the sink.
+        triggers(Triggers): Triggers of the CFG.
+
+    Returns:
+        A Vulnerability if it exists, else None
+    """
     if source.cfg_node in sink.cfg_node.new_constraint:
         source_trigger_word = source.trigger_word_tuple.trigger_word
         sink_trigger_word = sink.trigger_word_tuple.trigger_word
@@ -105,6 +187,15 @@ def get_vulnerability(source, sink, triggers):
     return None
 
 def find_vulnerabilities(cfg_list, trigger_word_file=default_trigger_word_file):
+    """Find vulnerabilities in a list of CFGs from a trigger_word_file.
+
+    Args:
+        cfg_list (list): the list of CFGs to scan.
+        trigger_word_file (string): file containing trigger words. Defaults to the flask trigger word file.
+
+    Returns:
+        A VulnerabilityLog with found vulnerabilities.
+    """
     definitions = parse(trigger_word_file)
     vulnerability_log = VulnerabilityLog()
     for cfg in cfg_list:
