8ddd8a1043a890813f4c9e5d717184d31e7db1bd
Stefan Micheelsen <stufkan@gmail.com>
2016-05-10 14:20:09 +0200

    test projecthandler methods

--- pyt/project_handler.py
+++ pyt/project_handler.py
@@ -16,16 +16,21 @@ def is_python_module(path):
     return False
 
 def get_python_modules(path):
-    module_root = path.split('/')[-1]
+    module_root = path.split('\\')[-1]
     modules = list()
     for root, directories, filenames in os.walk(path):
         for filename in filenames:
             if is_python_module(filename):
-                directory = os.path.dirname(os.path.realpath(os.path.join(root, filename))).split(module_root)[-1].replace('/', '')
-                if directory:
-                    modules.append(('.'.join((module_root, directory, filename.replace('.py', ''))), os.path.join(root, filename)))
-                else:
-                    modules.append(('.'.join((module_root, filename.replace('.py', ''))), os.path.join(root, filename)))
+
+                components = root.split('\\')
+                i = components.index(module_root)
+                directory = '.'.join(components[i:])
+
+                filename_no_py = filename.replace('.py', '')
+                         
+                filename_with_namespace = '.'.join((directory, filename_no_py))
+
+                modules.append((filename_with_namespace, os.path.join(root, filename)))
     return modules
 
 def get_project_module_names(path):
---
+++ tests/project_handler_test.py
@@ -0,0 +1,50 @@
+import os
+import sys
+import unittest
+from ast import parse
+
+sys.path.insert(0, os.path.abspath('../pyt'))
+from project_handler import is_python_module, get_python_modules
+from pprint import pprint
+
+class ProjectHandlerTest(unittest.TestCase):
+    """Tests for the project handler."""
+
+    def test_is_python_module(self):
+        python_module = './project_handler_test.py'
+        not_python_module = '../.travis.yml'
+        
+        self.assertEqual(is_python_module(python_module), True)
+        self.assertEqual(is_python_module(not_python_module), False)
+
+    def test_get_python_modules(self):
+        project_folder = os.path.normpath('../example\\test_project')
+        project_namespace = 'test_project'
+        folder = 'folder'
+        directory = 'directory'
+        
+        modules = get_python_modules(project_folder)
+
+        app_path = project_folder + '\\app.py'
+        utils_path = project_folder + '\\utils.py'
+        exceptions_path = project_folder + '\\exceptions.py'
+        some_path = project_folder + '\\' + folder + '\\some.py'
+        indhold_path = project_folder + '\\' + folder + '\\indhold.py'
+        
+        app_name = project_namespace + '.' + 'app'
+        utils_name = project_namespace + '.' + 'utils'
+        exceptions_name = project_namespace + '.' + 'exceptions'
+        some_name = project_namespace + '.' + folder + '.some'
+        indhold_name = project_namespace + '.' + folder + '.' + directory + '.indhold'
+        
+        app_tuple = (app_name, app_path)
+        utils_tuple = (utils_name, utils_path)
+        exceptions_tuple = (exceptions_name, exceptions_path)
+        some_tuple = (some_name, some_path)
+        
+        self.assertIn(app_tuple, modules)
+        self.assertIn(utils_tuple, modules)
+        self.assertIn(exceptions_tuple, modules)
+        self.assertIn(some_tuple, modules)
+
+        self.assertEqual(len(modules), 5)
