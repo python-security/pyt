ab6a418ac94426777015b8d80c17fab883af881d
Thalmann <mr.thalmann@gmail.com>
2016-05-11 13:01:58 +0200

    Flask adaptor fixed after functions was removed in cfg module

--- pyt/flask_adaptor.py
+++ pyt/flask_adaptor.py
@@ -1,23 +1,29 @@
 """Contains a class that can be used as adaptor."""
+import ast
+
 from framework_adaptor import FrameworkAdaptor
 
 
 class FlaskAdaptor(FrameworkAdaptor):
     """The flask adaptor class manipulates the CFG to adapt to flask applications."""
 
-    def is_flask_route_function(self, function):
+    def is_flask_route_function(self, ast_node):
         """Check whether function uses a decorator."""
-        return any(decorator for decorator in function.decorator_list if decorator.func.value.id == 'app' and decorator.func.attr == 'route')
+        for decorator in ast_node.decorator_list:
+            if isinstance(decorator, ast.Call):
+                if decorator.func.value.id == 'app' and decorator.func.attr == 'route':
+                    return True
+        return False
 
-    def find_flask_route_functions(self, functions):
+    def find_flask_route_functions(self, module_definitions):
         """Find all flask functions with decorators."""
-        for func in functions.items():
-            if self.is_flask_route_function(func[1]):
-                yield func[1]
+        for ast_node in module_definitions.items():
+            if self.is_flask_route_function(ast_node[1]):
+                yield ast_node[1]
 
     def run(self):
         """Executed by the super class, everything that needs to be executed goes here."""
         function_cfgs = list()
         for cfg in self.cfg_list:
-            function_cfgs.extend(self.find_flask_route_functions(cfg.functions))
+            function_cfgs.extend(self.find_flask_route_functions(cfg.module_definitions))
         self.cfg_list.extend(function_cfgs)
