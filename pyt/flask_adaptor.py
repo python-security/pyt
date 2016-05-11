ac97aac95240bbe0e004258bf33cccb3a9cd8b91
Thalmann <mr.thalmann@gmail.com>
2016-05-11 14:12:00 +0200

    Fixed flask adaptor

--- pyt/flask_adaptor.py
+++ pyt/flask_adaptor.py
@@ -2,7 +2,8 @@
 import ast
 
 from framework_adaptor import FrameworkAdaptor
-
+from cfg import CFG
+from project_handler import get_python_modules
 
 class FlaskAdaptor(FrameworkAdaptor):
     """The flask adaptor class manipulates the CFG to adapt to flask applications."""
@@ -11,19 +12,24 @@ class FlaskAdaptor(FrameworkAdaptor):
         """Check whether function uses a decorator."""
         for decorator in ast_node.decorator_list:
             if isinstance(decorator, ast.Call):
-                if decorator.func.value.id == 'app' and decorator.func.attr == 'route':
+                if decorator.func.attr == 'route':
                     return True
         return False
 
-    def find_flask_route_functions(self, module_definitions):
+    def get_cfg(self, ast_node, cfg):
+        cfg = CFG(cfg.project_modules)
+        cfg.create(ast_node)
+        return cfg
+
+    def find_flask_route_functions(self, cfg):
         """Find all flask functions with decorators."""
-        for ast_node in module_definitions.items():
+        for ast_node in cfg.module_definitions.items():
             if self.is_flask_route_function(ast_node[1]):
-                yield ast_node[1]
+                yield self.get_cfg(ast_node[1], cfg)
 
     def run(self):
         """Executed by the super class, everything that needs to be executed goes here."""
         function_cfgs = list()
         for cfg in self.cfg_list:
-            function_cfgs.extend(self.find_flask_route_functions(cfg.module_definitions))
+            function_cfgs.extend(self.find_flask_route_functions(cfg))
         self.cfg_list.extend(function_cfgs)
