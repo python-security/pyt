fcbb508e24f47b7af058be35b8956aca1205611e
Stefan Micheelsen <stufkan@gmail.com>
2016-05-14 11:03:44 +0200

    Revert "Removed unused visitor"
    This reverts commit 27a8b7293dfda9a58c7ff492e5aa5a0052927ba2.
    used for printing ast in report

---
+++ pyt/stacktrace_visitor.py
@@ -0,0 +1,9 @@
+from ast import NodeVisitor
+
+class StacktraceVisitor(NodeVisitor):
+    """Debug class that can be used to return information about nodes in the ast generated from the ast module."""
+
+    def visit(self,node):
+        """Visits all nodes and prints their class name."""
+        print(node.__class__.__name__)
+        self.generic_visit(node)
