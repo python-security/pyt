0febd1c688b4f04ab78b753c1e8b2467db9cfd5b
Thalmann <mr.thalmann@gmail.com>
2016-05-18 15:23:20 +0200

    Removed stupid oste shit
    Jeg var ved at lave det samme som logging modulet laver ..

--- pyt/log.py
+++ pyt/log.py
@@ -4,11 +4,6 @@ from collections import OrderedDict
 separator = ' | '
 log_formats = (('Date', '%(asctime)s'), ('path', '%(pathname)s'), ('filename', '%(filename)s'), ('module', '%(module)s'), ('line', '%(lineno)d'), ('function', '%(funcName)s'), ('level', '%(levelname)s'), ('message', '%(message)s'))
 log_format_dict = OrderedDict(log_formats)
-logger = None
-
-def set_logger(logger):
-    logger = logger
-    return logger
 
 def get_level(level):
     if level == None:
@@ -31,15 +26,12 @@ def get_level(level):
     else:
         raise TypeError('Unknown level type, should be: CRITICAL, ERROR, WARNING(Default), INFO, DEBUG or NOTSET. Level value is: ', level)
         
-def get_logger(level, name, show_path=True):
-    if logger:
-        return logger
+def set_logger(level, show_path=True):
     level = get_level(level)
     if not show_path:
         log_format_dict.pop('path')
     logging.basicConfig(filename='logger.log', level=level, format=separator.join(log_format_dict.values()))
-    l = set_logger(logging.getLogger(name))
-    l.info('FORMAT: ' + separator.join(log_format_dict.keys()))
-    l.info('Logging has been started.')
-    return l
+    logger = logging.getLogger(__name__)
+    logger.info('FORMAT: ' + separator.join(log_format_dict.keys()))
+    logger.info('Logging has been started.')
 
