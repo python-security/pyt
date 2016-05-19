3202a0f90ffefcf14e34d7df62febece5ff457af
Thalmann <mr.thalmann@gmail.com>
2016-05-19 15:09:51 +0200

    Docs for ast_helper module

--- pyt/ast_helper.py
+++ pyt/ast_helper.py
@@ -1,3 +1,4 @@
+"""This module contains helper function useful when working with the ast module."""
 import ast
 import os
 
@@ -9,6 +10,7 @@ def generate_ast(path):
     raise IOError('Input needs to be a file.')
 
 def list_to_dotted_string(list_of_components):
+    """Convert a list to a string seperated by a dot."""
     return '.'.join(list_of_components)
     
 def get_call_names_helper(node, result):
@@ -28,8 +30,10 @@ def get_call_names_helper(node, result):
         return get_call_names_helper(node.value, result)
 
 def get_call_names_as_string(node):
+    """Get a list of call names as a string."""
     return list_to_dotted_string(get_call_names(node))
 
 def get_call_names(node):
+    """Get a list of call names."""
     result = list()
     return reversed(get_call_names_helper(node, result))
