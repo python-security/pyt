1740cb8680b267a700492151b9d5c6a40699289b
Thalmann <mr.thalmann@gmail.com>
2016-05-19 16:13:27 +0200

    Forgot to commit for test of imports

---
+++ example/example_inputs/import.py
@@ -0,0 +1,4 @@
+import import_os
+import import_ast as a
+import import_math as _
+
---
+++ example/import_test/import_from.py
@@ -0,0 +1,3 @@
+from import_os import foo as f
+from import_ast import bar as b
+from import_math import num as n
