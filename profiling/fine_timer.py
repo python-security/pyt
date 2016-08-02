67aa9e53ee48c692167d29a3d9dcdc9d0992a07a
Thalmann <mr.thalmann@gmail.com>
2016-08-02 09:56:07 +0200

    visualiser for timing

--- profiling/fine_timer.py
+++ profiling/fine_timer.py
@@ -5,8 +5,7 @@ from subprocess import run as sub_run, PIPE
 python = 'python3'
 pyt_path = '../pyt/pyt.py'
 stats_filename = 'stats'
-
-
+snakeviz = 'snakeviz'
 
 def clean_up():
     if os.path.isfile(stats_filename):
@@ -16,6 +15,16 @@ def prepare_results(number_of_results):
     stats = pstats.Stats(stats_filename)
     stats.sort_stats('cumulative')
     stats.print_stats(number_of_results)
+
+def visualise():
+    try:
+        sub_run([snakeviz, stats_filename])
+    except KeyboardInterrupt:
+        pass
+    except:
+        print('It seems that snakeviz is not installed.')
+        print('To install snakeviz see: https://jiffyclub.github.io/snakeviz/ .')
+        exit(0)
 
 def run(project, project_file, number_of_results):
     if project:
--- profiling/profiler.py
+++ profiling/profiler.py
@@ -7,6 +7,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('project_file', help='Path to the file where PyT should start to analyse.', type=str)
 parser.add_argument('-pr', '--project', help='Path to where the project is located which PyT should analyse.', type=str)
 parser.add_argument('-n', '--number-of-results', help='Number of results to be shown. Default: 10.', type=int)
+parser.add_argument('-v', '--visualise', help='Visualise the results in an interactive way.', action='store_true')
 
 args = parser.parse_args()
 
@@ -15,4 +16,8 @@ if args.number_of_results:
     number_of_results = args.number_of_results
 
 fine_timer.run(args.project, args.project_file, number_of_results)
+
+if args.visualise:
+    fine_timer.visualise()
+
 fine_timer.clean_up()
