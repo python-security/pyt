27b7f17f0c4196485a7e755dfb958cb28fa937c2
Thalmann <mr.thalmann@gmail.com>
2016-08-10 07:55:47 +0200

    Profiling runner saving to file

--- profiling/profiling_runner.py
+++ profiling/profiling_runner.py
@@ -2,18 +2,29 @@
 
 Saves the result for future reference.
 """
-from subprocess import Popen, PIPE
+from subprocess import Popen
+from datetime import datetime
 
 TRAVIS_PYTHON = 'python'
 PROFILER = 'profiler.py'
 TEST_PROJECT_1 = 'test_projects/flaskbb_lite_1/flaskbb/app.py'
 TEST_PROJECT_2 = 'test_projects/flaskbb_lite_2/flaskbb/app.py'
 TEST_PROJECT_3 = 'test_projects/flaskbb_lite_3/flaskbb/app.py'
+TEST_PROJECTS = [TEST_PROJECT_1]#,TEST_PROJECT_2, TEST_PROJECT_3]
+FIXED_POINT_FLAG = '-fp'
+PROFILING_DB = 'db.txt'
 
-with open('saved.txt', 'a') as fd:
-    p = Popen(['python3', PROFILER, TEST_PROJECT_1], stdout=fd)
-with open('saved.txt', 'a') as fd:
-    p = Popen(['python3', PROFILER, TEST_PROJECT_2], stdout=fd)
-with open('saved.txt', 'a') as fd:
-    p = Popen(['python3', PROFILER, TEST_PROJECT_3], stdout=fd)
+with open(PROFILING_DB, 'a') as fd:
+    fd.write('############################################### Profiling \
+{} ###############################################\n\n'.format(datetime.now()))
 
+for tp in TEST_PROJECTS:
+    print('Profiling: ', tp)        
+    with open(PROFILING_DB, 'a') as fd:
+        fd.write('##### Profiling {} #####\n\n'.format(tp))
+    with open(PROFILING_DB, 'a') as fd:
+        p = Popen(['python3', PROFILER, tp, FIXED_POINT_FLAG], stdout=fd)
+        p.wait()
+
+with open(PROFILING_DB, 'a') as fd:
+    fd.write('\n')
