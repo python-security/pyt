7caea7b27b99e36ca50d56f0ff4bbc2836d6eed9
Thalmann <mr.thalmann@gmail.com>
2016-08-30 09:44:30 +0200

    Bit vector lattice implemented
    And also unit tested

--- pyt/lattice.py
+++ pyt/lattice.py
@@ -1,92 +1,107 @@
-"""This module implements a lattice to use with static analysis."""
-from itertools import combinations
+from cfg import AssignmentNode
+
 
 class Lattice:
-    def __init__(self, elements, relation):
-        self.elements = elements # Should be a set as a lattice is a partial order
-        self.relation = relation
-        self.generate_elements()
 
-    def generate_elements(self):
-        l = list()
-        for x in range(0, len(self.elements)+1):
-            for e in combinations(self.elements, x):
-                l.append(LatticeElement(e))
-        self.elements = l
+    def __init__(self, elements, cfg_nodes):
+        self.d = dict()
+        self.l = list()
+        for i, e in enumerate(elements):
+            self.d[e] = 0b1 << i
+            self.l.append(e)
+        self.l = list(reversed(self.l))
+        self.table = dict()
+        for node in cfg_nodes:
+            self.table[node] = 0b0
 
-    def join(self, e1, e2): # least upper bound
-        #Save upper bound on lattice element - speed over memory?
-        e1_upper_bounds = list()
-        e2_upper_bounds = list()
-        for e in self.elements:
-            if self.relation(e1, e):
-                e1_upper_bounds.append(e)
-            if self.relation(e2, e):
-                e2_upper_bounds.append(e)
-        #print([str(e) for e  in e1_upper_bounds])
-        #print([str(e) for e  in e2_upper_bounds])
-        common_upper_bounds = set(e1_upper_bounds).intersection(e2_upper_bounds)
-        return min(common_upper_bounds, key=len)
+    def meet(self, iterable1, iterable2):
+        r1 = self.constraint_join(iterable1)
+        r2 = self.constraint_join(iterable2)
+        return r1 & r2
 
-    def meet(self, e1, e2): # least upper bound
-        #Save lower bound on lattice element - speed over memory?
-        e1_lower_bounds = list()
-        e2_lower_bounds = list()
-        for e in self.elements:
-            if self.relation(e, e1):
-                e1_lower_bounds.append(e)
-            if self.relation(e, e2):
-                e2_lower_bounds.append(e)
-        #print([str(e) for e  in e1_lower_bounds])
-        #print([str(e) for e  in e2_lower_bounds])
-        common_lower_bounds = set(e1_lower_bounds).intersection(e2_lower_bounds)
-        return max(common_lower_bounds, key=len)
-    
-    def smallest_element(self):
-        return min(self.elements, key=len)
+    def simple_meet(self, iterable):
+        r = self.d[iterable[0]]
+        for e in iterable:
+            r = r & self.table[e]
+        return r
 
-    def largest_element(self):
-        return max(self.elements, key=len)
+    def constraint_meet(self, iterable):
+        r = self.d[iterable[0]]
+        for e in iterable:
+            r = r & self.table[e]
+        return r
 
-    def __str__(self):
-        return ', '.join([str(e) for e in self.elements])
+    def join(self, iterable1, iterable2):
+        r1 = self.constraint_join(iterable1)
+        r2 = self.constraint_join(iterable2)
+        return r1 | r2
 
-    def __iter__(self):
-        return iter(self.elements)
+    def simple_join(self, iterable):
+        r = 0
+        for e in iterable:
+            r = r | self.d[e]
+        return r
 
-class LatticeElement:
-    def __init__(self, elements):
-        self.elements = elements
-        # Questionable:
-        self.upper_bounds = None 
-        self.lower_bounds = None
-        self.least_upper_bound = None
-        self.greatest_lower_bound = None
+    def constraint_join(self, iterable):
+        r = 0
+        for e in iterable:
+            r = r | self.table[e]
+        return r
 
-    def __str__(self):
-        return '{' + ', '.join([str(e) for e in self.elements]) + '}'
+    def get_elements(self, number):
+        r = list()
 
-    def __len__(self):
-        return len(self.elements)
+        if number == 0:
+            return r
 
-    def __iter__(self):
-        return iter(self.elements)
+        for i, x in enumerate(format(number, '0' + str(len(self.l)) + 'b')):
+            if x == '1':
+                r.append(self.l[i])
+        return r
+
+    def has_element(self, node1, node2):
+        """Checks if node1 is in node2's constraints
+        For instance node1 = 010 and node2 = 110:
+        010 & 110 = 010 -> has the element."""
+        constraint = self.table[node2]
+
+        try:
+            value = self.d[node1] #if node1 in self.d else 0b0
+        except KeyError:
+            value = 0b0
+
+        if constraint == 0b0 or value == 0b0:
+            return False
+
+        return constraint & value != 0
+
+    def __getitem__(self, key):
+        try:
+            return self.table[key]
+        except KeyError:
+            print('KeyError FUCK')
+            return None
 
 
-def subset_relation(e1, e2):
-    return set(e1.elements).issubset(set(e2.elements))
+def generate_lattices(cfg_list):
+    lattices = list()
+    for cfg in cfg_list:
+        # Takes only assignment nodes for lattice elements atm - this should be paramterized
+        lattices.append(Lattice([node for node in cfg.nodes if isinstance(node, AssignmentNode)], cfg.nodes))
+    return lattices
+
 
 if __name__ == '__main__':
-    l = Lattice([1,2,3,4], subset_relation)
-    print(l)
-    print(l.join(LatticeElement([1, 2]), LatticeElement([2, 3])))
-    print(l.meet(LatticeElement([1, 2]), LatticeElement([2, 3])))
-    print(l.smallest_element())
-    print(l.largest_element())
-
-    l = Lattice(['x = 1', 'x = x + 1', 'y = 3', 'y = z'], subset_relation)
-    print(l)
-    print(l.join(LatticeElement(['x = 1', 'y = z']), LatticeElement(['x = 1', 'y = 3'])))
-    print(l.meet(LatticeElement(['x = 1', 'y = z']), LatticeElement(['x = 1', 'y = 3'])))
-    print(l.smallest_element())
-    print(l.largest_element())
+    from sys import getsizeof as gso
+    l = Lattice(['a', 'b', 'c']) # Consider duplicates
+    print(l.d['a'])
+    print(bin(l.join(['a'], ['b'])))
+    print(l.d)
+    print([bin(x) for x in l.d.values()])
+    print(bin(l.meet(['a', 'b'], ['b', 'c'])))
+    print(l.l)
+    print(l.get_elements(4))
+    input()
+    l = Lattice([*range(10000)])
+    print(gso(l.d)/1000000, 'mb')
+    print(gso(l.l)/1000000, 'mb')
--- tests/lattice_test.py
+++ tests/lattice_test.py
@@ -3,77 +3,116 @@ import os
 
 from base_test_case import BaseTestCase
 sys.path.insert(0, os.path.abspath('../pyt'))
-from lattice import Lattice, LatticeElement, subset_relation
+from lattice import Lattice
 
 class LatticeTest(BaseTestCase):
 
-    def is_lattice_element_equal(self, e1, e2):
-        for x in range(len(e1)):
-            if e1.elements[x] != e2.elements[x]:
-                return False
-        return True
-
-    def same_elements(self, l1, l2):
-        for x in range(len(l1)):
-            if not self.is_lattice_element_equal(l1[x], l2[x]):
-                return False
-        return True
-
-    def assert_element_equal(self, e1, e2):
-        self.assertEqual(self.is_lattice_element_equal(e1, e2), True)
-
     def test_generate_integer_elements(self):
-        lattice = Lattice([1,2,3], subset_relation)
+        lattice = Lattice([1,2,3], ['a', 'b', 'c'])
 
-        elements = list()
-        for i in [(), tuple([1]), tuple([2]), tuple([3]), (1,2), (1,3), (2,3), (1,2,3)]:
-            elements.append(LatticeElement(i))
+        self.assertEqual(lattice.d[1], 0b1)
+        self.assertEqual(lattice.d[2], 0b10)
+        self.assertEqual(lattice.d[3], 0b100)
 
-        self.assertEqual(self.same_elements(lattice.elements, elements), True)
+        self.assertEqual(lattice.table['a'], 0b0)
+        self.assertEqual(lattice.table['b'], 0b0)
+        self.assertEqual(lattice.table['c'], 0b0)
 
-    def test_generate_string_elements(self):
-        a = 'x = 1'
-        b = 'y = 2'
-        c = 'x = y'
-        lattice = Lattice([a, b, c], subset_relation)
-
-        elements = list()
-        for i in [(), tuple([a]), tuple([b]), tuple([c]), (a,b), (a,c), (b,c), (a,b,c)]:
-            elements.append(LatticeElement(i))
-
-        self.assertEqual(self.same_elements(lattice.elements, elements), True)
+        self.assertEqual(lattice.l[0], 3)
+        self.assertEqual(lattice.l[1], 2)
+        self.assertEqual(lattice.l[2], 1)
 
     def test_join(self):
         a = 'x = 1'
-        b = 'y = 2'
-        c = 'x = y'
-        lattice = Lattice([a, b, c], subset_relation)
-        self.assert_element_equal(lattice.join(LatticeElement([a]), LatticeElement([b])), LatticeElement((a, b)))
+        b = 'print(x)'
+        c = 'x = 3'
+        d = 'y = x'
+        
+        lattice = Lattice([a, c, d], [a, b, c, d])
 
-    def test_meet1(self):
-        a = 'x = 1'
-        b = 'y = 2'
-        c = 'x = y'
-        lattice = Lattice([a, b, c], subset_relation)
-        self.assert_element_equal(lattice.meet(LatticeElement([a,b]), LatticeElement([b,c])), LatticeElement([b]))
+        # Constraint results after fixpoint:
+        lattice.table[a] = 0b0001
+        lattice.table[b] = 0b0001
+        lattice.table[c] = 0b0010
+        lattice.table[d] = 0b1010
 
-    def test_meet2(self):
-        a = 'x = 1'
-        b = 'y = 2'
-        c = 'x = y'
-        lattice = Lattice([a, b, c], subset_relation)
-        self.assert_element_equal(lattice.meet(LatticeElement([a,b]), LatticeElement([a,b,c])), LatticeElement([a, b]))
+        r = lattice.join([a,c], [c])
+        self.assertEqual(r, 0b11)
+        r = lattice.join([a, c], [d, c])
+        self.assertEqual(r, 0b1011)
+        r = lattice.join([a], [c])
+        self.assertEqual(r, 0b11)
+        r = lattice.join([c], [d])
+        self.assertEqual(r, 0b1010)
+        r = lattice.join([], [a])
+        self.assertEqual(r, 0b1)
+        r = lattice.join([a,c,d], [a,c,d])
+        self.assertEqual(r, 0b1011)
+        r = lattice.join([d,c], [])
+        self.assertEqual(r, 0b1010)
 
-    def test_largest_element(self):
+    def test_meet(self):
         a = 'x = 1'
-        b = 'y = 2'
-        c = 'x = y'
-        lattice = Lattice([a, b, c], subset_relation)
-        self.assert_element_equal(lattice.largest_element(), LatticeElement([a,b,c]))
+        b = 'print(x)'
+        c = 'x = 3'
+        d = 'y = x'
+        
+        lattice = Lattice([a, c, d], [a, b, c, d])
 
-    def test_smallest_element(self):
+        # Constraint results after fixpoint:
+        lattice.table[a] = 0b0001
+        lattice.table[b] = 0b0001
+        lattice.table[c] = 0b0010
+        lattice.table[d] = 0b1010
+
+        r = lattice.meet([a,c], [c,d])
+        self.assertEqual(r, 0b10)
+        r = lattice.meet([a], [d])
+        self.assertEqual(r, 0b0)
+        r = lattice.meet([a,c,d], [a,c])
+        self.assertEqual(r, 0b011)
+        r = lattice.meet([c,d], [a,d])
+        self.assertEqual(r, 0b1010)
+        r = lattice.meet([], [])
+        self.assertEqual(r, 0b0)
+        r = lattice.meet([a], [])
+        self.assertEqual(r, 0b0)
+
+    def test_has_element(self):
         a = 'x = 1'
-        b = 'y = 2'
-        c = 'x = y'
-        lattice = Lattice([a, b, c], subset_relation)
-        self.assert_element_equal(lattice.smallest_element(), LatticeElement([]))
+        b = 'print(x)'
+        c = 'x = 3'
+        d = 'y = x'
+        
+        lattice = Lattice([a, c, d], [a, b, c, d])
+        lattice.table[a] = 0b001
+        lattice.table[b] = 0b001
+        lattice.table[c] = 0b010
+        lattice.table[d] = 0b110
+
+        self.assertEqual(lattice.has_element(a, b), True)
+        self.assertEqual(lattice.has_element(a, a), True)
+        self.assertEqual(lattice.has_element(a, d), False)
+        self.assertEqual(lattice.has_element(a, c), False)
+        self.assertEqual(lattice.has_element(c, d), True)
+        self.assertEqual(lattice.has_element(d, d), True)
+        self.assertEqual(lattice.has_element(c, c), True)
+        self.assertEqual(lattice.has_element(c, a), False)
+        self.assertEqual(lattice.has_element(c, b), False)
+
+    def test_get_elements(self):
+        a = 'x = 1'
+        b = 'print(x)'
+        c = 'x = 3'
+        d = 'y = x'
+        
+        lattice = Lattice([a, c, d], [a, b, c, d])
+
+        self.assertEqual(set(lattice.get_elements(0b111)), {a,c,d})
+        self.assertEqual(set(lattice.get_elements(0b0)), set())
+        self.assertEqual(set(lattice.get_elements(0b1)), {a})
+        self.assertEqual(set(lattice.get_elements(0b10)), {c})
+        self.assertEqual(set(lattice.get_elements(0b100)), {d})
+        self.assertEqual(set(lattice.get_elements(0b11)), {a,c})
+        self.assertEqual(set(lattice.get_elements(0b101)), {a,d})
+        self.assertEqual(set(lattice.get_elements(0b110)), {c,d})
