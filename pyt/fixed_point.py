909c565fc81547debf1c7bccc8ff9621dc423359
Thalmann <mr.thalmann@gmail.com>
2016-08-30 09:46:29 +0200

    Fixpoint changed to so it works with the new lattice

--- pyt/fixed_point.py
+++ pyt/fixed_point.py
@@ -8,38 +8,34 @@ from liveness import LivenessAnalysis
 class FixedPointAnalysis():
     """Run the fix point analysis."""
 
-    def __init__(self, cfg, analysis):
+    def __init__(self, cfg, analysis, lattice):
         """Fixed point analysis
 
         analysis must be a dataflow analysis containing a 'fixpointmethod' method that analyzes one CFG node"""
-        self.analysis = analysis(cfg)
+        self.analysis = analysis(cfg, lattice)
         self.cfg = cfg
-    
-    def constraints_changed(self):
-        """Return true if any constraint has changed."""
-        return any(node.old_constraint != node.new_constraint for node in self.cfg.nodes)
-        
-    def swap_constraints(self):
-        """Set odl constraint to new constraint and set new constraint to None."""
-        for node in self.cfg.nodes:
-            node.old_constraint = node.new_constraint
-            node.new_constraint = None
+        self.lattice = lattice
 
     def fixpoint_runner(self):
         """Work list algorithm that runs the fixpoint algorithm."""
         q = self.cfg.nodes
 
         while q != []:
+            x_i = self.lattice.table[q[0]] # to get old constraint has to be here before fixpointmethod
+
             # y = F_i(x_1, ..., x_n):
             self.analysis.fixpointmethod(q[0])
-            y = q[0].new_constraint
-            x_i = q[0].old_constraint
+            #y = q[0].new_constraint
+            y = self.lattice.table[q[0]]
+            #x_i = q[0].old_constraint
+
 
             if y != x_i:
                 # for (v in dep(v_i)) q.append(v):
                 for node in self.analysis.dep(q[0]):
                     q.append(node)
-                q[0].old_constraint = q[0].new_constraint # x_1 = y
+                #q[0].old_constraint = q[0].new_constraint # x_1 = y
+                self.lattice.table[q[0]] = y
             q = q[1:] # q = q.tail()
 
     def fixpoint_iteration(self):
@@ -48,10 +44,10 @@ class FixedPointAnalysis():
             self.analysis.fixpointmethod(node)
             
 
-def analyse(cfg_list, *, analysis_type):
+def analyse(cfg_list, lattices, *, analysis_type):
     """Analyse a list of control flow graphs with a given analysis type."""
-    for cfg in cfg_list:
-        analysis = FixedPointAnalysis(cfg, analysis_type)
+    for i, cfg in enumerate(cfg_list):
+        analysis = FixedPointAnalysis(cfg, analysis_type, lattices[i])
         analysis.fixpoint_runner()
 
 if __name__ == '__main__':
