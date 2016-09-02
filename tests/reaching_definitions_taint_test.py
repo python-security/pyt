76a8397463c61dc8837f55ae6722f36776d3528f
Thalmann <mr.thalmann@gmail.com>
2016-09-02 08:48:18 +0200

    Duplicate method commited by mistake

--- tests/reaching_definitions_taint_test.py
+++ tests/reaching_definitions_taint_test.py
@@ -14,21 +14,6 @@ class ReachingDefinitionsTaintTest(BaseTestCase):
     def setUp(self):
         self.cfg = None
 
-    def assertInCfg2(self, connections):
-        ''' Assert that all connections in the connections list exists in the cfg,
-        as well as all connections not in the list do not exist
-
-        connections is a list of tuples where the node at index 0 of the tuple has to be in the new_constraintset of the node a index 1 of the tuple'''
-        for connection in connections:
-            self.assertIn(self.cfg.nodes[connection[0]], self.cfg.nodes[connection[1]].new_constraint, str(connection) + " expected to be connected")
-
-        nodes = len(self.cfg.nodes)
-        
-        for element in range(nodes):
-            for sets in range(nodes):
-                if (element, sets) not in connections:
-                    self.assertNotIn(self.cfg.nodes[element], self.cfg.nodes[sets].new_constraint, "(%s,%s)" % (element, sets)  +  " expected to be disconnected")
-
     def assertInCfg(self, connections, lattice):
         ''' Assert that all connections in the connections list exists in the cfg,
         as well as all connections not in the list do not exist
