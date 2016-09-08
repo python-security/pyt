23fcd5a477e32e0402c3cd1785381a850a69fe55
Thalmann <mr.thalmann@gmail.com>
2016-09-08 13:13:31 +0200

    Liveness analysis works with test
    First draft should be re-factored
    Also not tested with vulnerable code

--- pyt/liveness.py
+++ pyt/liveness.py
@@ -1,74 +1,122 @@
-"""Module implements liveness analysis."""
-from cfg import AssignmentNode
-from copy import deepcopy
-from ast import NodeVisitor, Compare, Call
+import ast
 
+from cfg import AssignmentNode, EntryExitNode
 from analysis_base import AnalysisBase
+from lattice import Lattice
+from constraint_table import constraint_table, constraint_join
+from ast_helper import get_call_names_as_string
+from right_hand_side_visitor import RHSVisitor
+
+
+lattice_elements = list()
 
 class LivenessAnalysis(AnalysisBase):
-    """Implement liveness analysis rules."""
+    """Reaching definitions analysis rules implemented."""
 
     def __init__(self, cfg):
-        """Initialize using parent with the given cfg."""
-        super(LivenessAnalysis, self).__init__(cfg, VarsVisitor)
-    
-    def join(self, cfg_node):
-        """Join outgoing old constraints and return them as a set."""
-        JOIN = set()
-        for outgoing in cfg_node.outgoing:
-            if outgoing.old_constraint:
-                JOIN |= outgoing.old_constraint
-        return JOIN
-    
-    def fixpointmethod(self, cfg_node):
-        """Setting the constraints of the given cfg node obeying the liveness analysis rules."""
-        # if for Condition and call case: Join(v) u vars(E).
-        if cfg_node.ast_type == Compare.__name__ or cfg_node.ast_type == Call.__name__:
-            JOIN = self.join(cfg_node)            
-            JOIN.update(self.annotated_cfg_nodes[cfg_node])  # set union
-            
-            cfg_node.new_constraint = JOIN
+        super(LivenessAnalysis, self).__init__(cfg, None)
 
-        # if for Assignment case: Join(v) \ {id} u vars(E).
-        elif isinstance(cfg_node, AssignmentNode): 
+    def join(self, cfg_node):
+        """Joins all constraints of the ingoing nodes and returns them.
+        This represents the JOIN auxiliary definition from Schwartzbach."""
+        return constraint_join(cfg_node.outgoing)
+
+    def is_output(self, cfg_node):
+        if isinstance(cfg_node.ast_node, ast.Call):
+            call_name = get_call_names_as_string(cfg_node.ast_node.func)
+            if 'print' in call_name:
+                return True
+        return False
+
+    def remove_id(self):
+        pass
+
+    @staticmethod
+    def get_variables(cfg_node):
+        try:
+            return [node.id for node in ast.walk(cfg_node.ast_node) if isinstance(node, ast.Name)]
+        except AttributeError:
+            return list()
+
+    def fixpointmethod(self, cfg_node):
+        
+        if isinstance(cfg_node, EntryExitNode) and 'Exit' in cfg_node.label:
+            constraint_table[cfg_node] = 0
+        elif isinstance(cfg_node, AssignmentNode):
+            lvars = list()
+            try:
+                for expr in cfg_node.ast_node.targets:
+                    rhsv = RHSVisitor()
+                    rhsv.visit(expr)
+                    lvars.extend(rhsv.result)
+            except AttributeError:
+                rhsv = RHSVisitor()
+                rhsv.visit(cfg_node.ast_node.value)
+                lvars.extend(rhsv.result)
+
             JOIN = self.join(cfg_node)
 
-            JOIN.discard(cfg_node.ast_node.targets[0].id)  # set difference
-            JOIN.update(self.annotated_cfg_nodes[cfg_node])  # set union
- 
-            cfg_node.new_constraint = JOIN
+            for var in lvars:
+                if var in self.lattice.get_elements(JOIN):
+                    JOIN = JOIN ^ self.lattice.el2bv[var]
 
-        # if for entry and exit cases: {}.
-        elif cfg_node.ast_type == "ENTRY" or cfg_node.ast_type == "EXIT":
-            pass
-        # else for other cases.
+            for var in cfg_node.right_hand_side_variables:
+                JOIN = JOIN | self.lattice.el2bv[var]
+
+            constraint_table[cfg_node] = JOIN
+        elif isinstance(cfg_node.ast_node, ast.Compare) or isinstance(cfg_node.ast_node, ast.While) or self.is_output(cfg_node):
+            varse = None
+            if isinstance(cfg_node.ast_node, ast.While):
+                rhsv = RHSVisitor()
+                rhsv.visit(cfg_node.ast_node.test)
+                varse = rhsv.result
+            elif self.is_output(cfg_node):
+                rhsv = RHSVisitor()
+                rhsv.visit(cfg_node.ast_node)
+                varse = rhsv.result
+            
+            JOIN = self.join(cfg_node)
+
+            for var in varse:
+                JOIN = JOIN | self.lattice.el2bv[var]
+
+            constraint_table[cfg_node] = JOIN
         else:
-            cfg_node.new_constraint = self.join(cfg_node)
+            constraint_table[cfg_node] = self.join(cfg_node)
 
+    def dep(self, q_1):
+        """Represents the dep mapping from Schwartzbach."""
+        for node in q_1.outgoing:
+            yield node
 
-class VarsVisitor(NodeVisitor):
-    """Class that finds all variables needed for the liveness analysis."""
+    def get_lattice_elements(cfg_nodes):
+        """Returns all assignment nodes as they are the only lattice elements 
+        in the reaching definitions analysis.
+        This is a static method which is overwritten from the base class.
+        """
+        for node in (node for node in cfg_nodes if node.ast_node):
+            vv = VarsLatticeVisitor()
+            vv.visit(node.ast_node)
+            for var in vv.result:
+                if var not in lattice_elements:
+                    lattice_elements.append(var)
+        return lattice_elements
+
+    def equal(self, value, other):
+        return value == other
+
+    def build_lattice(self, cfg):
+        self.lattice = Lattice(cfg.nodes, LivenessAnalysis)
+
+class VarsLatticeVisitor(ast.NodeVisitor):
 
     def __init__(self):
-        """Initialise list of results."""
         self.result = list()
+        self.call = False
 
     def visit_Name(self, node):
-        self.result.append(node.id)
+        if not self.call:
+            self.result.append(node.id)
 
-    #  Condition and call rule
     def visit_Call(self, node):
-        for arg in node.args:
-            self.visit(arg)
-        for keyword in node.keywords:
-            self.visit(keyword)
-            
-    def visit_keyword(self, node):
-        self.visit(node.value)
-
-    def visit_Compare(self, node):
-        self.generic_visit(node)
-
-    #  Assignment rule                
-    def visit_Assign(self, node): 
-        self.visit(node.value)
+        self.call = True
---
+++ tests/liveness_test.py
@@ -0,0 +1,31 @@
+import sys
+import os
+
+from analysis_base_test_case import AnalysisBaseTestCase
+sys.path.insert(0, os.path.abspath('../pyt'))
+from liveness import LivenessAnalysis
+from constraint_table import constraint_table
+
+class LivenessTest(AnalysisBaseTestCase):
+    def test_example(self):
+        lattice = self.run_analysis('../example/example_inputs/example.py', LivenessAnalysis)
+
+        x = 0b1
+        y = 0b10
+        z = 0b100
+
+        self.assertEqual(constraint_table[self.cfg.nodes[0]], 0)
+        self.assertEqual(constraint_table[self.cfg.nodes[1]], 0)
+        self.assertEqual(constraint_table[self.cfg.nodes[2]], x)
+        self.assertEqual(constraint_table[self.cfg.nodes[3]], x)
+        self.assertEqual(constraint_table[self.cfg.nodes[4]], x)
+        self.assertEqual(constraint_table[self.cfg.nodes[5]], x | y)
+        self.assertEqual(constraint_table[self.cfg.nodes[6]], x | y)
+        self.assertEqual(constraint_table[self.cfg.nodes[7]], x)
+        self.assertEqual(constraint_table[self.cfg.nodes[8]], x | z)
+        self.assertEqual(constraint_table[self.cfg.nodes[9]], x | z)
+        self.assertEqual(constraint_table[self.cfg.nodes[10]], x | z)
+        self.assertEqual(constraint_table[self.cfg.nodes[11]], x)
+        self.assertEqual(constraint_table[self.cfg.nodes[12]], 0)
+
+        self.assertEqual(len(lattice.el2bv), 3)
