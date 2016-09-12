ce8a196ac7a1cc39e037b17f7901d0edfc261174
Thalmann <mr.thalmann@gmail.com>
2016-09-12 15:46:57 +0200

    Fixed point clean up pep8 grr

--- pyt/fixed_point.py
+++ pyt/fixed_point.py
@@ -1,10 +1,6 @@
 """This module implements the fixed point algorithm."""
-import argparse
-
-from cfg import CFG, generate_ast
-from reaching_definitions import ReachingDefinitionsAnalysis
-from liveness import LivenessAnalysis
 from constraint_table import constraint_table
+
 
 class FixedPointAnalysis():
     """Run the fix point analysis."""
@@ -12,7 +8,8 @@ class FixedPointAnalysis():
     def __init__(self, cfg, analysis):
         """Fixed point analysis
 
-        analysis must be a dataflow analysis containing a 'fixpointmethod' method that analyzes one CFG node"""
+        analysis must be a dataflow analysis containing a 'fixpointmethod'
+        method that analyzes one CFG node"""
         self.analysis = analysis(cfg)
         self.cfg = cfg
 
@@ -21,22 +18,21 @@ class FixedPointAnalysis():
         q = self.cfg.nodes
 
         while q != []:
-            x_i = constraint_table[q[0]] # to get old constraint has to be here before fixpointmethod
+            x_i = constraint_table[q[0]]
 
             # y = F_i(x_1, ..., x_n):
             self.analysis.fixpointmethod(q[0])
-            #y = q[0].new_constraint
+            # y = q[0].new_constraint
             y = constraint_table[q[0]]
-            #x_i = q[0].old_constraint
-
+            # x_i = q[0].old_constraint
 
             if not self.analysis.equal(y, x_i):
                 # for (v in dep(v_i)) q.append(v):
                 for node in self.analysis.dep(q[0]):
                     q.append(node)
-                #q[0].old_constraint = q[0].new_constraint # x_1 = y
+                # q[0].old_constraint = q[0].new_constraint # x_1 = y
                 constraint_table[q[0]] = y
-            q = q[1:] # q = q.tail()
+            q = q[1:]  # q = q.tail()
 
 
 def analyse(cfg_list, *, analysis_type):
@@ -44,29 +40,3 @@ def analyse(cfg_list, *, analysis_type):
     for cfg in cfg_list:
         analysis = FixedPointAnalysis(cfg, analysis_type)
         analysis.fixpoint_runner()
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    
-    parser.add_argument('filename', help='Filename of the file that should be analysed.', type=str)
-    parser.add_argument('-l', '--liveness', help='Toggle liveness analysis.', action='store_true')
-    parser.add_argument('-r', '--reaching', help='Toggle reaching definitions analysis', action='store_true')
-
-    args = parser.parse_args()
-
-    tree = generate_ast(args.filename)
-    cfg = CFG()
-    cfg.create(tree)
-
-    def run_analysis(cfg, analysis_type):
-        analysis = fixed_point_analysis(cfg, analysis_type)
-        analysis.fixpoint_runner()
-        for cfg_node in cfg.nodes:
-            print(cfg_node)
-            print(cfg_node.new_constraint)
-
-    if args.liveness:
-        run_analysis(cfg, LivenessAnalysis)
-    if args.reaching:
-        run_analysis(cfg, ReachingDefinitionsAnalysis)
-
