efde5b82c7ad0941e6bbe4fb820e0ddb6256a2e9
Thalmann <mr.thalmann@gmail.com>
2016-09-12 08:48:17 +0200

    Added vars visitor + test

---
+++ pyt/vars_visitor.py
@@ -0,0 +1,123 @@
+import ast
+
+class VarsVisitor(ast.NodeVisitor):
+    def __init__(self):
+        self.result = list()
+
+    def visit_Name(self, node):
+        self.result.append(node.id)
+
+    def visit_BoolOp(self, node):
+        for v in node.values:
+            self.visit(v)
+
+    def visit_BinOp(self, node):
+        self.visit(node.left)
+        self.visit(node.right)
+
+    def visit_UnaryOp(self, node):
+        self.visit(node.operand)
+
+    def visit_Lambda(self, node):
+        self.visit(node.body)
+
+    def visit_IfExpr(self, node):
+        self.visit(node.test)
+        self.visit(node.body)
+        self.visit(node.orelse)
+
+    def visit_Dict(self, node):
+        for k in node.keys:
+            self.visit(k)
+        for v in node.values:
+            self.visit(v)
+
+    def visit_Set(self, node):
+        for e in node.elts:
+            self.visit(e)
+
+    def comprehension(self, node):
+        self.visit(node.target)
+        self.visit(node.iter)
+        for c in node.ifs:
+            self.visit(c)
+
+    def visit_ListComp(self, node):
+        self.visit(node.elt)
+        for gen in node.generators:
+            self.comprehension(gen)
+            
+    def visit_SetComp(self, node):
+        self.visit(node.elt)
+        for gen in node.generators:
+            self.comprehension(gen)
+
+    def visit_DictComp(self, node):
+        self.visit(node.key)
+        self.visit(node.value)
+        for gen in node.generators:
+            self.comprehension(gen)
+
+    def visit_GeneratorComp(self, node):
+        self.visit(node.elt)
+        for gen in node.generators:
+            self.comprehension(gen)
+
+    def visit_Await(self, node):
+        self.visit(value)
+
+    def visit_Yield(self, node):
+        if node.value:
+            self.visit(node.value)
+
+    def visit_YieldFrom(self, node):
+        self.visit(node.value)
+
+    def visit_Compare(self, node):
+        self.visit(node.left)
+        for c in node.comparators:            
+            self.visit(c)
+
+    def visit_Call(self, node):
+        if not isinstance(node.func, ast.Name):
+            self.visit(node.func)
+        if node.args:
+            for arg in node.args:
+                self.visit(arg)
+        if node.keywords:
+            for keyword in node.keywords:
+                self.visit(keyword)
+
+    def visit_Attribute(self, node):
+        if not isinstance(node.value, ast.Name):
+            self.visit(node.value)
+
+    def slicev(self, node):
+        if isinstance(node, ast.Slice):
+            if node.lower:
+                self.visit(node.lower)
+            if node.upper:
+                self.visit(node.upper)
+            if node.step:
+                self.visit(node.step)
+        elif isinstance(node, ast.ExtSlice):
+            if node.dims:
+                for d in node.dims:
+                    self.visit(d)
+        else:
+            self.visit(node.value)
+
+    def visit_Subscript(self, node):
+        self.visit(node.value)
+        self.slicev(node.slice)
+
+    def visit_Starred(self, node):
+        self.visit(node.value)
+
+    def visit_List(self, node):
+        for el in node.elts:
+            self.visit(el)
+
+    def visit_Tuple(self, node):
+        for el in node.elts:
+            self.visit(el)
---
+++ tests/vars_visitor_test.py
@@ -0,0 +1,52 @@
+import os
+import sys
+import unittest
+from ast import parse
+
+sys.path.insert(0, os.path.abspath('../pyt'))
+from vars_visitor import VarsVisitor
+
+
+class VarsVisitorTestCase(unittest.TestCase):
+    '''Baseclass for VarsVisitor tests'''
+
+    def perform_vars_on_expression(self, expr):
+        obj = parse(expr)
+        vars = VarsVisitor()
+        vars.visit(obj)
+
+        return vars
+
+class VarsVisitorTest(VarsVisitorTestCase):
+    def test_assign_var_and_num(self):
+        vars = self.perform_vars_on_expression('a = 1')
+        self.assertEqual(vars.result,['a'])
+
+    def test_assign_var_and_var(self):
+        vars = self.perform_vars_on_expression('a = x')
+        self.assertEqual(vars.result,['a', 'x'])
+
+    def test_call1(self):
+        vars = self.perform_vars_on_expression('print(x)')
+        self.assertEqual(vars.result,['x'])
+
+    def test_call2(self):
+        vars = self.perform_vars_on_expression('print.print(x)')
+        self.assertEqual(vars.result,['x'])
+
+    def test_call3(self):
+        vars = self.perform_vars_on_expression('print.print.x(y).s(x)')
+        self.assertEqual(vars.result,['y','x'])
+
+
+    def test_keyword_vararg(self):
+        vars = self.perform_vars_on_expression('print(arg = x)')
+        self.assertEqual(vars.result,['x'])
+
+    def test_keyword_numarg(self):
+        vars = self.perform_vars_on_expression('print(arg = 1)')
+        self.assertEqual(vars.result,[])
+
+    def test_subscript(self):
+        vars = self.perform_vars_on_expression('l[a] = x + y')
+        self.assertEqual(vars.result,['l', 'a', 'x', 'y'])
