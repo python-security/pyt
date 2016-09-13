c611fa2eb01127013a375dde0a56f18d5bd0ffae
Thalmann <mr.thalmann@gmail.com>
2016-09-13 08:22:29 +0200

    print project modules not local modules

--- pyt/pyt.py
+++ pyt/pyt.py
@@ -35,7 +35,7 @@ analysis_group.add_argument('-li', '--liveness', help='Run liveness analysis. De
 analysis_group.add_argument('-re', '--reaching', help='Run reaching definitions analysis. Default is reaching definitions tainted version.', action='store_true')
 analysis_group.add_argument('-rt', '--reaching-taint', help='This is the default analysis: reaching definitions tainted version.', action='store_true')
 parser.add_argument('-intra', '--intraprocedural-analysis', help='Run intraprocedural analysis.', action='store_true')
-parser.add_argument('-plm', '--print-local-modules', help='Print local modules.', action='store_true')
+parser.add_argument('-ppm', '--print-project-modules', help='Print project modules.', action='store_true')
 
 args = parser.parse_args()
 
@@ -104,7 +104,7 @@ if __name__ == '__main__':
             print('############## CFG number: ', i)
             print(repr(e))
 
-    if args.print_local_modules:
+    if args.print_project_modules:
         from pprint import pprint
         print('############## PROJECT MODULES ##############')
         pprint(project_modules)
