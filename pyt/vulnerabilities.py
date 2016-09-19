d5333197e565cbbc6731a62dd646acb46e180e60
Thalmann <mr.thalmann@gmail.com>
2016-09-19 09:54:23 +0200

    pep8

--- pyt/vulnerabilities.py
+++ pyt/vulnerabilities.py
@@ -17,14 +17,18 @@ Triggers = namedtuple('Triggers', 'sources sinks sanitiser_dict')
 Sanitiser = namedtuple('Sanitiser', 'trigger_word cfg_node')
 Definitions = namedtuple('Definitions', 'sources sinks')
 
-default_trigger_word_file = os.path.join(os.path.dirname(__file__), 'trigger_definitions', 'flask_trigger_words.pyt')
+default_trigger_word_file = os.path.join(os.path.dirname(__file__),
+                                         'trigger_definitions',
+                                         'flask_trigger_words.pyt')
 
 SANITISER_SEPARATOR = '->'
 SOURCES_KEYWORD = 'sources:'
 SINKS_KEYWORD = 'sinks:'
 
+
 class TriggerNode():
-    def __init__(self, trigger_word, sanitisers, cfg_node, secondary_nodes=None):
+    def __init__(self, trigger_word, sanitisers, cfg_node,
+                 secondary_nodes=None):
         self.trigger_word = trigger_word
         self.sanitisers = sanitisers
         self.cfg_node = cfg_node
@@ -36,6 +40,7 @@ class TriggerNode():
                 self.secondary_nodes.append(cfg_node)
             elif not self.secondary_nodes:
                 self.secondary_nodes = [cfg_node]
+
 
 def parse_section(iterator):
     """Parse a section of a file. Stops at empty line.
@@ -60,7 +65,8 @@ def parse_section(iterator):
             line = next(iterator).rstrip()
     except StopIteration:
         return
-        
+
+
 def parse(trigger_word_file=default_trigger_word_file):
     """Parse the file for source and sink definitions.
 
@@ -77,13 +83,15 @@ def parse(trigger_word_file=default_trigger_word_file):
             elif line == SINKS_KEYWORD:
                 sinks = list(parse_section(fd))
     return Definitions(sources, sinks)
-        
+
+
 def identify_triggers(cfg, sources, sinks):
     """Identify sources, sinks and sanitisers in a CFG.
 
     Args:
         cfg(CFG): CFG to find sources, sinks and sanitisers in.
-        sources(tuple): list of sources, a source is a (source, sanitiser) tuple.
+        sources(tuple): list of sources,
+        a source is a (source, sanitiser) tuple.
         sinks(tuple): list of sources, a sink is a (sink, sanitiser) tuple.
 
     Returns:
@@ -91,22 +99,24 @@ def identify_triggers(cfg, sources, sinks):
     """
     assignment_nodes = filter_cfg_nodes(cfg, AssignmentNode)
     tainted_nodes = filter_cfg_nodes(cfg, TaintedNode)
-    tainted_trigger_nodes = [TriggerNode('Flask function URL parameter', None, node) for node in tainted_nodes]
+    tainted_trigger_nodes = [TriggerNode('Flask function URL parameter', None,
+                                         node) for node in tainted_nodes]
 
-    
     sources_in_file = find_triggers(assignment_nodes, sources)
     sources_in_file.extend(tainted_trigger_nodes)
-    
+
     find_secondary_sources(assignment_nodes, sources_in_file)
 
     sinks_in_file = find_triggers(cfg.nodes, sinks)
-       
+
     sanitiser_node_dict = build_sanitiser_node_dict(cfg, sinks_in_file)
-    
+
     return Triggers(sources_in_file, sinks_in_file, sanitiser_node_dict)
+
 
 def filter_cfg_nodes(cfg, cfg_node_type):
     return [node for node in cfg.nodes if isinstance(node, cfg_node_type)]
+
 
 def find_secondary_sources(assignment_nodes, sources):
     assignments = dict()
@@ -114,22 +124,27 @@ def find_secondary_sources(assignment_nodes, sources):
     for source in sources:
         source.secondary_nodes = find_assignments(assignment_nodes, source)
 
+
 def find_assignments(assignment_nodes, source):
     old = list()
-    new = [source.cfg_node] # added in order to propagate reassignments of the source node
+
+    # added in order to propagate reassignments of the source node:
+    new = [source.cfg_node]
 
     update_assignments(new, assignment_nodes, source.cfg_node)
     while new != old:
         old = new
         update_assignments(new, assignment_nodes, source.cfg_node)
-    new.remove(source.cfg_node) # remove source node from result
+    new.remove(source.cfg_node)  # remove source node from result
     return new
+
 
 def update_assignments(l, assignment_nodes, source):
     for node in assignment_nodes:
         for other in l:
             if node not in l:
                 append_if_reassigned(l, other, node)
+
 
 def append_if_reassigned(l, secondary, node):
     # maybe:  secondary in node.new_constraint and
@@ -141,7 +156,8 @@ def append_if_reassigned(l, secondary, node):
         print(secondary)
         print('EXCEPT' + secondary)
         exit(0)
-    
+
+
 def find_triggers(nodes, trigger_words):
     """Find triggers from the trigger_word_list in the cfg.
 
@@ -156,7 +172,8 @@ def find_triggers(nodes, trigger_words):
     for node in nodes:
         l.extend(iter(label_contains(node, trigger_words)))
     return l
-      
+
+
 def label_contains(node, trigger_words):
     """Determine if node contains any of the trigger_words provided.
 
@@ -165,7 +182,8 @@ def label_contains(node, trigger_words):
         trigger_words(list[string]): list of trigger words to look for.
 
     Returns:
-        Iterable of TriggerNodes found. Can be multiple because multiple trigger_words can be in one node.
+        Iterable of TriggerNodes found. Can be multiple because multiple
+        trigger_words can be in one node.
     """
     for trigger_word_tuple in trigger_words:
         if trigger_word_tuple[0] in node.label:
@@ -173,12 +191,15 @@ def label_contains(node, trigger_words):
             sanitisers = trigger_word_tuple[1]
             yield TriggerNode(trigger_word, sanitisers, node)
 
+
 def build_sanitiser_node_dict(cfg, sinks_in_file):
-    """Build a dict of string -> TriggerNode pairs, where the string is the sanitiser and the TriggerNode is a TriggerNode of the sanitiser.
+    """Build a dict of string -> TriggerNode pairs, where the string
+       is the sanitiser and the TriggerNode is a TriggerNode of the sanitiser.
 
     Args:
         cfg(CFG): cfg to traverse.
-        sinks_in_file(list[TriggerNode]): list of TriggerNodes contains the sinks in the file
+        sinks_in_file(list[TriggerNode]): list of TriggerNodes contains
+        the sinks in the file
 
     Returns:
         A string -> TriggerNode dict.
@@ -195,8 +216,10 @@ def build_sanitiser_node_dict(cfg, sinks_in_file):
 
     sanitiser_node_dict = dict()
     for sanitiser in sanitisers:
-        sanitiser_node_dict[sanitiser] = list(find_sanitiser_nodes(sanitiser, sanitisers_in_file))
+        sanitiser_node_dict[sanitiser] = list(find_sanitiser_nodes(sanitiser,
+                                                                   sanitisers_in_file))
     return sanitiser_node_dict
+
 
 def find_sanitiser_nodes(sanitiser, sanitisers_in_file):
     """Find nodes containing a particular sanitiser.
@@ -208,9 +231,10 @@ def find_sanitiser_nodes(sanitiser, sanitisers_in_file):
     Returns:
         Iterable of sanitiser nodes.
     """
-    for sanitiser_tuple  in sanitisers_in_file:
+    for sanitiser_tuple in sanitisers_in_file:
         if sanitiser == sanitiser_tuple.trigger_word:
             yield sanitiser_tuple.cfg_node
+
 
 def is_sanitized(sink, sanitiser_dict, lattice):
     """Check if sink is sanitised by any santiser in the sanitiser_dict.
@@ -219,7 +243,7 @@ def is_sanitized(sink, sanitiser_dict, lattice):
         sink(TriggerNode): TriggerNode of the sink.
         sanitiser_dict(dict): dictionary of sink sanitiser pairs.
 
-    Returns: 
+    Returns:
         True of False
     """
     for sanitiser in sink.sanitisers:
@@ -227,6 +251,7 @@ def is_sanitized(sink, sanitiser_dict, lattice):
             if lattice.in_constraint(cfg_node, sink.cfg_node):
                 return True
     return False
+
 
 def get_sink_args(cfg_node):
     if type(cfg_node) == AssignmentNode:
@@ -258,7 +283,8 @@ def get_sink_args(cfg_node):
                 args.append(arg.value)
             elif isinstance(arg, ast.NameConstant):
                 args.append(arg.value)
-            elif isinstance(arg, ast.Attribute) or isinstance(arg, ast.Subscript):
+            elif isinstance(arg, ast.Attribute) or isinstance(arg,
+                                                              ast.Subscript):
                 import ast_helper
                 args.append(ast_helper.get_call_names_as_string(arg))
             else:
@@ -287,12 +313,17 @@ def get_vulnerability(source, sink, triggers, lattice):
 
     secondary_in_sink = []
     if source.secondary_nodes:
-        secondary_in_sink = [secondary for secondary in source.secondary_nodes if lattice.in_constraint(secondary, sink.cfg_node)]
+        secondary_in_sink = [secondary for secondary in source.secondary_nodes
+                             if lattice.in_constraint(secondary,
+                                                      sink.cfg_node)]
     trigger_node_in_sink = source_in_sink or secondary_in_sink
     
     sink_args = get_sink_args(sink.cfg_node)
-    source_lhs_in_sink_args = source.cfg_node.left_hand_side in sink_args if sink_args else None
-    secondary_nodes_in_sink_args = any(True for node in secondary_in_sink if node.left_hand_side in sink_args) if sink_args else None
+    source_lhs_in_sink_args = source.cfg_node.left_hand_side in sink_args\
+                              if sink_args else None
+    secondary_nodes_in_sink_args = any(True for node in secondary_in_sink
+                                       if node.left_hand_side in sink_args)\
+                                        if sink_args else None
     lhs_in_sink_args = source_lhs_in_sink_args or secondary_nodes_in_sink_args
 
     logger.debug('Checking for vuln:')
@@ -302,13 +333,20 @@ def get_vulnerability(source, sink, triggers, lattice):
     if trigger_node_in_sink and lhs_in_sink_args:
         source_trigger_word = source.trigger_word
         sink_trigger_word = sink.trigger_word
-        sink_is_sanitised = is_sanitized(sink, triggers.sanitiser_dict, lattice)
+        sink_is_sanitised = is_sanitized(sink, triggers.sanitiser_dict,
+                                         lattice)
 
-        if not sink_is_sanitised :
-            return Vulnerability(source.cfg_node, source_trigger_word, sink.cfg_node, sink_trigger_word, source.secondary_nodes)
+        if not sink_is_sanitised:
+            return Vulnerability(source.cfg_node, source_trigger_word,
+                                 sink.cfg_node, sink_trigger_word,
+                                 source.secondary_nodes)
         elif sink_is_sanitised:
-            return SanitisedVulnerability(source.cfg_node, source_trigger_word, sink.cfg_node, sink_trigger_word, sink.sanitisers, source.secondary_nodes)
+            return SanitisedVulnerability(source.cfg_node, source_trigger_word,
+                                          sink.cfg_node, sink_trigger_word,
+                                          sink.sanitisers,
+                                          source.secondary_nodes)
     return None
+
 
 def find_vulnerabilities_in_cfg(cfg, vulnerability_log, definitions, lattice):
     """Find vulnerabilities in a cfg.
@@ -325,20 +363,23 @@ def find_vulnerabilities_in_cfg(cfg, vulnerability_log, definitions, lattice):
             if vulnerability:
                 vulnerability_log.append(vulnerability)
 
-def find_vulnerabilities(cfg_list, analysis_type, trigger_word_file=default_trigger_word_file):
+
+def find_vulnerabilities(cfg_list, analysis_type,
+                         trigger_word_file=default_trigger_word_file):
     """Find vulnerabilities in a list of CFGs from a trigger_word_file.
 
     Args:
         cfg_list (list): the list of CFGs to scan.
-        trigger_word_file (string): file containing trigger words. Defaults to the flask trigger word file.
+        trigger_word_file (string): file containing trigger words.
+        Defaults to the flask trigger word file.
 
     Returns:
         A VulnerabilityLog with found vulnerabilities.
     """
     definitions = parse(trigger_word_file)
-    
+
     vulnerability_log = VulnerabilityLog()
     for cfg in cfg_list:
-        find_vulnerabilities_in_cfg(cfg, vulnerability_log, definitions, Lattice(cfg.nodes, analysis_type))
+        find_vulnerabilities_in_cfg(cfg, vulnerability_log, definitions,
+                                    Lattice(cfg.nodes, analysis_type))
     return vulnerability_log
-   
