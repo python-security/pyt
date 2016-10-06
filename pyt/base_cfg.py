9f7a8db66638d4e7134586ebe7c30d300d18f870
Thalmann <mr.thalmann@gmail.com>
2016-10-06 10:31:27 +0200

    Attribute returned None cause not handled
    For instance instance.attribute on one line did return None cause its
    visitor was not implemented
    This solves one case in issue #44

--- pyt/base_cfg.py
+++ pyt/base_cfg.py
@@ -650,3 +650,9 @@ class Visitor(ast.NodeVisitor):
         label_visitor.visit(node.test)
 
         return self.append_node(Node(label_visitor.result, node, line_number = node.lineno, path=self.filenames[-1]))
+
+    def visit_Attribute(self, node):
+        label_visitor = LabelVisitor()
+        label_visitor.visit(node)
+
+        return self.append_node(Node(label_visitor.result, node, line_number = node.lineno, path=self.filenames[-1]))
