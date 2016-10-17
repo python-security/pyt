58ea568d6805966a5ce8e8ef612c096ec5d1f62b
Thalmann <mr.thalmann@gmail.com>
2016-10-17 17:19:55 +0200

    pep8

--- pyt/framework_adaptor.py
+++ pyt/framework_adaptor.py
@@ -1,9 +1,12 @@
-"""A framework adaptor is a adaptor used to adapt the source code to a specific framework."""
+"""A framework adaptor is a adaptor used to adapt
+the source code to a specific framework."""
 from abc import ABCMeta, abstractmethod
 from base_cfg import AssignmentNode
 
+
 class FrameworkAdaptor(metaclass=ABCMeta):
-    """An engine that should be used as base class to specify how to find all sources and sinks."""
+    """An engine that should be used as base class
+    to specify how to find all sources and sinks."""
 
     def __init__(self, cfg_list, project_modules, local_modules):
         self.cfg_list = cfg_list
@@ -14,6 +17,7 @@ class FrameworkAdaptor(metaclass=ABCMeta):
     @abstractmethod
     def run(self):
         pass
+
 
 class TaintedNode(AssignmentNode):
     pass
