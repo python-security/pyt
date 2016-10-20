e1a9a653de3c716361a62822e50e350e0f1e4bb3
Thalmann <mr.thalmann@gmail.com>
2016-10-20 09:17:57 +0200

    pep8-ify

--- tests/run.py
+++ tests/run.py
@@ -10,22 +10,34 @@ encoding = 'utf-8'
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-py', '--python', help='Specify Python 3.', type=str)
-parser.add_argument('-s', '--save-results', help='Add new results', action='store_true')
-parser.add_argument('-p', '--pyt-output', help='Print output of PyT for each file.', action='store_true')
+parser.add_argument('-s', '--save-results',
+                    help='Add new results', action='store_true')
+parser.add_argument('-p', '--pyt-output',
+                    help='Print output of PyT for each file.',
+                    action='store_true')
 
-files = ['XSS.py', 'command_injection.py', 'path_traversal.py', 'path_traversal_sanitised.py', 'sql/sqli.py', 'XSS_form.py', 'XSS_url.py', 'XSS_no_vuln.py', 'XSS_reassign.py', 'XSS_sanitised.py', 'XSS_variable_assign_no_vuln.py', 'XSS_variable_assign.py', 'XSS_variable_multiple_assign.py']
+files = ['XSS.py', 'command_injection.py', 'path_traversal.py',
+         'path_traversal_sanitised.py', 'sql/sqli.py', 'XSS_form.py',
+         'XSS_url.py', 'XSS_no_vuln.py', 'XSS_reassign.py',
+         'XSS_sanitised.py', 'XSS_variable_assign_no_vuln.py',
+         'XSS_variable_assign.py', 'XSS_variable_multiple_assign.py']
 files = [example_file_path + filename for filename in files]
+
 
 def check_files(python):
     try:
         with open(results_file, 'r', encoding=encoding) as fd:
             results = fd.read().split(delimiter)
     except FileNotFoundError:
-        print(results_file + ' file was not found. Generate this file by running this script with option --save-results or -s.')
+        print(results_file + ' file was not found.'
+              ' Generate this file by running this'
+              ' script with option --save-results or -s.')
         exit(1)
 
-    results.pop() # last element is empty because results file always ends with a delimiter
-        
+    # last element is empty because
+    # results file always ends with a delimiter:
+    results.pop()
+
     passed = True
 
     if len(results) != len(files):
@@ -44,6 +56,7 @@ def check_files(python):
             passed = False
     return passed
 
+
 def save_results(python):
     with open(results_file, 'w', encoding=encoding) as fd:
         for f in files:
@@ -52,6 +65,7 @@ def save_results(python):
             fd.write(str(process.stdout))
             fd.write(delimiter)
             print('Saved result to file: "' + results_file + '".')
+
 
 def print_pyt_output(python):
     for f in files:
