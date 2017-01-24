2eddbbbd6aa04ae0737cc43681a8a5ed05baeb37
Stefan Micheelsen <stufkan@gmail.com>
2017-01-24 21:49:32 +0100

    add argumentHelper for validating dates in cmd arguments

---
+++ pyt/argument_helpers.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from argparse import ArgumentTypeError
+
+def valid_date(s):
+    date_format = "%Y-%m-%d"
+    try:
+        return datetime.strptime(s, date_format).date()
+    except ValueError:
+        msg = "Not a valid date: '{0}'. Format: {1}".format(s, date_format)
+        raise ArgumentTypeError(msg)
