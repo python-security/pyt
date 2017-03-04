41317a51f98d6166e06d79730ffc99cda5312c1f
Stefan Micheelsen <smi@netcompany.com>
2017-03-04 17:04:38 +0100

    testcases for get_dates

--- tests/github_search_test.py
+++ tests/github_search_test.py
@@ -1,11 +1,41 @@
 import unittest
+
 from datetime import date
 
 from pyt.github_search import get_dates
 
 
-class GetDatesTest(unittest.TestCase):
-    def test_range_shorter_than_interval(self):
-        date_range = get_dates(date(2016,12,12), date(2016,12,13), 7)
+class TestGetDates(unittest.TestCase):
+    def assertDateTuple(self, dateInterval, expectedStart, expectedEnd):
+        print(dateInterval)
+        print(expectedStart)
+        print(expectedEnd)
 
+        assert(dateInterval[0] == expectedStart)
+        assert(dateInterval[1] == expectedEnd)
 
+    def stest_range_shorter_than_interval(self):
+        date_range = get_dates(date(2016, 12, 12), date(2016, 12, 13), 7)
+        date_range = list(date_range)
+
+        print(date_range)
+        self.assertDateTuple(date_range[0],
+                             date(2016, 12, 12),
+                             date(2016, 12, 13))
+
+    def test_range_longer_than_interval(self):
+        date_range = get_dates(date(2016, 12, 12), date(2016, 12, 16), 2)
+        date_range = list(date_range)
+
+        print(date_range)
+        self.assertDateTuple(date_range[0],
+                             date(2016, 12, 12),
+                             date(2016, 12, 13))
+
+        self.assertDateTuple(date_range[1],
+                             date(2016, 12, 14),
+                             date(2016, 12, 15))
+
+        self.assertDateTuple(date_range[2],
+                             date(2016, 12, 16),
+                             date(2016, 12, 16))
