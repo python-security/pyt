fe37645382721288e813b95c311c185cef7eb8bd
Thalmann <mr.thalmann@gmail.com>
2017-03-05 08:32:19 +0100

    Issue #3 | Entry points added to setup.py.

--- setup.py
+++ setup.py
@@ -1,36 +1,41 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 long_description = """"""
 
 setup(
-        name='pyt',
-        version='1.0.0a6',
-        description='Find security vulnerabilities in Python web applications'
-        ' using static analysis.',
-        long_description=long_description,
-        url='https://github.com/python-security/pyt',
-        author='python-security',
-        author_email='mr.thalmann@gmail.com',
-        license='GPLv2',
-        classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Intended Audience :: Developers',
-            'Intended Audience :: Science/Research',
-            'Topic :: Security',
-            'Topic :: Software Development',
-            'Topic :: Scientific/Engineering',
-            'Topic :: Utilities',
-            'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-            'Programming Language :: Python :: 3.4'
-        ],
-        keywords='security vulnerability web flask django pyt static analysis',
-        packages=find_packages(exclude=['example',
-                                        'profiling',
-                                        'scan_results',
-                                        'tests']),
-        entry_points={
-            'console_scripts': [
-                'pyt = pyt.pyt:main'
-            ]
-        }
+    name='pyt',
+    version='1.0.0a20 ',
+    description='Find security vulnerabilities in Python web applications'
+    ' using static analysis.',
+    long_description=long_description,
+    url='https://github.com/python-security/pyt',
+    author='python-security',
+    author_email='mr.thalmann@gmail.com',
+    license='GPLv2',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Security',
+        'Topic :: Software Development',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Programming Language :: Python :: 3.5'
+    ],
+    keywords='security vulnerability web flask django pyt static analysis',
+    packages=[
+        'pyt'
+    ],
+    install_requres=[
+        'graphviz==0.4.10',
+        'requests==2.10.0',
+        'GitPython==2.0.8'
+    ],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'pyt = pyt:main'
+        ]
+    }
 )
