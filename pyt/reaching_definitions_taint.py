c2028f10b9d4d4bcd0260f8d5893e954df56b778
Thalmann <mr.thalmann@gmail.com>
2017-05-13 12:49:25 +0200

    Refactor: Added base class for reaching definitions and reaching definitions taint analysis. This removes some code duplication.

--- pyt/reaching_definitions_taint.py
+++ pyt/reaching_definitions_taint.py
@@ -1,28 +1,10 @@
-from .analysis_base import AnalysisBase
 from .base_cfg import AssignmentNode
-from .constraint_table import constraint_join, constraint_table
-from .lattice import Lattice
+from .constraint_table import constraint_table
+from .reaching_definitions_base import ReachingDefinitionsAnalysisBase
 
 
-class ReachingDefinitionsTaintAnalysis(AnalysisBase):
+class ReachingDefinitionsTaintAnalysis(ReachingDefinitionsAnalysisBase):
     """Reaching definitions analysis rules implemented."""
-
-    def __init__(self, cfg):
-        super().__init__(cfg, None)
-
-    def join(self, cfg_node):
-        """Joins all constraints of the ingoing nodes and returns them.
-        This represents the JOIN auxiliary definition from Schwartzbach."""
-        return constraint_join(cfg_node.ingoing)
-
-    def arrow(self, JOIN, _id):
-        """Removes all assignments from JOIN that has _id on the left hand side.
-        This represents the arrow id definition from Schwartzbach."""
-        r = JOIN
-        for node in self.lattice.get_elements(JOIN):
-            if node.left_hand_side == _id.left_hand_side:
-                r = r ^ self.lattice.el2bv[node]
-        return r
 
     def fixpointmethod(self, cfg_node):
         JOIN = self.join(cfg_node)
@@ -40,23 +22,3 @@ class ReachingDefinitionsTaintAnalysis(AnalysisBase):
         # Default case:
         else:
             constraint_table[cfg_node] = JOIN
-
-    def dep(self, q_1):
-        """Represents the dep mapping from Schwartzbach."""
-        for node in q_1.outgoing:
-            yield node
-
-    def get_lattice_elements(cfg_nodes):
-        """Returns all assignment nodes as they are the only lattice elements
-        in the reaching definitions analysis.
-        This is a static method which is overwritten from the base class.
-        """
-        for node in cfg_nodes:
-            if isinstance(node, AssignmentNode):
-                yield node
-
-    def equal(self, value, other):
-        return value == other
-
-    def build_lattice(self, cfg):
-        self.lattice = Lattice(cfg.nodes, ReachingDefinitionsTaintAnalysis)
