1f4759af970f66c9a6a89faf714bf7a122eef7be
Kevin Hock <kevin.hock.opentoall@gmail.com>
2017-07-12 20:14:01 -0400

    [whitespace] remove trailing whitespace

--- pyt/__main__.py
+++ pyt/__main__.py
@@ -73,7 +73,7 @@ def parse_args(args):
                         help='Input trigger word file.', type=str)
     parser.add_argument('-py2', '--python-2',
                         help='[WARNING, EXPERIMENTAL] Turns on Python 2 mode,' +
-                        ' needed when target file(s) are written in Python 2.', action='store_true')    
+                        ' needed when target file(s) are written in Python 2.', action='store_true')
     parser.add_argument('-l', '--log-level',
                         help='Choose logging level: CRITICAL, ERROR,' +
                         ' WARNING(Default), INFO, DEBUG, NOTSET.', type=str)
