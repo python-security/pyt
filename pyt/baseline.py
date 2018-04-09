c4cd04c1ffe79a1a64a2efb96e88aed40a2af1ff
Ömer Günal <o.gunal977@gmail.com>
2018-04-09 23:02:59 +0300

    Create baseline.py
    just for checking error. it's not totally done

---
+++ pyt/baseline.py
@@ -0,0 +1,27 @@
+from pprint import pprint
+import json
+
+
+def isSame(res, base):
+	if res == base:
+		return(True)
+	return(False)
+
+def compare(results, baseline):
+
+	baseline = json.load(open(baseline))
+	results = json.load(open(results))
+	result = {'generated_at':results["generated_at"], 'vulnerabilities':[]}
+
+	if "generated_at" in results and baseline:
+		if not isSame(results["generated_at"], baseline["generated_at"]):
+			pprint(results["generated_at"])
+
+	if "vulnerabilities" in results and baseline:
+		if not isSame(results["vulnerabilities"], baseline["vulnerabilities"]):
+			for i in range(len(results["vulnerabilities"])):
+				if results["vulnerabilities"][i] not in baseline["vulnerabilities"]:
+					result["vulnerabilities"].append(results["vulnerabilities"][i])
+
+	result = json.dumps(result, indent=4)
+	print(result)
