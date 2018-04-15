41f378930b4b666dea0fe4670cb2eae81eec664f
Kevin Hock <kevin.hock.opentoall@gmail.com>
2018-04-14 18:14:05 -0700

    Move django_open_source_apps.csv, flask_open_source_apps.csv, func_counter.py, make_dependency_graph.sh, profiling/ to readthedocs repo

--- profiling/test_projects/flaskbb_lite_3/docs/theming.rst
+++
@@ -1,4 +0,0 @@
-.. _theming:
-
-Theming (WIP)
-=============
--- profiling/test_projects/flaskbb_lite_3/wsgi.py
+++
@@ -1,4 +0,0 @@
-from flaskbb import create_app
-from flaskbb.configs.production import ProductionConfig
-
-flaskbb = create_app(config=ProductionConfig())
