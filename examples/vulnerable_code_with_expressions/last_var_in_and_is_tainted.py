c6d9e62111b1d671aa2afe5b822c557a833d2d75
Kevin Hock <kevin.hock.opentoall@gmail.com>
2018-07-09 19:01:29 -0700

    Add vulnerable_code_with_expressions/last_var_in_and_is_tainted and last_var_in_and_is_not_tainted with test

---
+++ examples/vulnerable_code_with_expressions/last_var_in_and_is_tainted.py
@@ -0,0 +1,6 @@
+"""
+This is very similar to one of the false-negatives found in our November 16th/17th 2017 evaluation
+http://pyt.readthedocs.io/en/latest/past_evaluations.html#november-16-17th-2017
+"""
+
+redirect(request.args.get('The') if 'hey' or 'you' else request.args.get('French') if 'foo' else 'c' and request.args.get('Aces'))
---
+++ tests/cfg/expr_cfg_test.py
@@ -0,0 +1,78 @@
+from .cfg_base_test_case import CFGBaseTestCase
+
+from pyt.core.node_types import (
+    EntryOrExitNode,
+    Node
+)
+
+
+class CFGExprTest(CFGBaseTestCase):
+    def test_last_and_not_tainted(self):
+        self.cfg_create_from_file('examples/vulnerable_code_with_expressions/last_var_in_and_is_not_tainted.py')
+
+        self.assert_length(self.cfg.nodes, expected_length=9)
+
+        entry = 0
+        first_if_exp_test_if_Hey_or_You = 1
+        call_2_req_get_The = 2
+        second_if_exp_test_if_Foo = 3
+        call_3_req_get_French = 4
+        and_bool_op = 5
+        call_4_req_get_Aces = 6
+        call_1_ret_redirect = 7
+        _exit = 8
+
+        self.assertInCfg([
+            (first_if_exp_test_if_Hey_or_You, entry),
+
+            (call_2_req_get_The, first_if_exp_test_if_Hey_or_You),
+            (second_if_exp_test_if_Foo, first_if_exp_test_if_Hey_or_You),
+
+            (call_1_ret_redirect, call_2_req_get_The),
+
+            (call_3_req_get_French, second_if_exp_test_if_Foo),
+            (and_bool_op, second_if_exp_test_if_Foo),
+
+            (call_1_ret_redirect, call_3_req_get_French),
+
+            (call_4_req_get_Aces, and_bool_op),
+
+            # call_4_req_get_Aces.outgoing == []
+
+            (_exit, call_1_ret_redirect)
+        ])
+
+    def test_last_and_tainted(self):
+        self.cfg_create_from_file('examples/vulnerable_code_with_expressions/last_var_in_and_is_tainted.py')
+
+        self.assert_length(self.cfg.nodes, expected_length=9)
+
+        entry = 0
+        first_if_exp_test_if_Hey_or_You = 1
+        call_2_req_get_The = 2
+        second_if_exp_test_if_Foo = 3
+        call_3_req_get_French = 4
+        and_bool_op = 5
+        call_4_req_get_Aces = 6
+        call_1_ret_redirect = 7
+        _exit = 8
+
+        self.assertInCfg([
+            (first_if_exp_test_if_Hey_or_You, entry),
+
+            (call_2_req_get_The, first_if_exp_test_if_Hey_or_You),
+            (second_if_exp_test_if_Foo, first_if_exp_test_if_Hey_or_You),
+
+            (call_1_ret_redirect, call_2_req_get_The),
+
+            (call_3_req_get_French, second_if_exp_test_if_Foo),
+            (and_bool_op, second_if_exp_test_if_Foo),
+
+            (call_1_ret_redirect, call_3_req_get_French),
+
+            (call_4_req_get_Aces, and_bool_op),
+
+            (call_1_ret_redirect, call_4_req_get_Aces),  # Connected this time
+
+            (_exit, call_1_ret_redirect)
+        ])
